d5381cc76a318ee861876dc1960482eb299f1b06
Mafinar Khan <mafinar@gmail.com>
2018-03-11 18:23:33 -0400

    Year: 2015, Day: 2

--- lib/2015/day_1.ex
+++ lib/2015/day_1.ex
@@ -1,8 +1,9 @@
 defmodule AdventOfCode.Y2015.Day1 do
-  def input!(), do: AdventOfCode.Data.InputReader.read!(2015, 1)
   def floor(""), do: 0
   def floor("(" <> rest), do: 1 + floor(rest)
   def floor(")" <> rest), do: -1 + floor(rest)
+
+  defp input!(), do: AdventOfCode.Data.InputReader.read!(2015, 1)
 
   def run do
     input!() |> floor()
---
+++ lib/2015/day_2.ex
@@ -0,0 +1,36 @@
+defmodule AdventOfCode.Y2015.Day2 do
+  def surface_area(sides) do
+    sides
+    |> area_per_dim()
+    |> Enum.sum()
+    |> Kernel.*(2)
+  end
+
+  def area_per_dim([width, length, height]) do
+    [length * width, width * height, height * length]
+  end
+
+  def minimum([side_1, side_2, side_3]) do
+    side_1 |> min(side_2) |> min(side_3)
+  end
+
+  def required_paper([_, _, _] = sides) do
+    (sides |> surface_area()) + (sides |> area_per_dim() |> minimum())
+  end
+
+  defp input!(), do: AdventOfCode.Data.InputReader.read!(2015, 2)
+
+  defp parse_input(lines) do
+    lines
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, "x"))
+  end
+
+  def run do
+    input!()
+    |> parse_input()
+    |> Enum.map(fn n -> n |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(&required_paper/1)
+    |> Enum.sum()
+  end
+end
