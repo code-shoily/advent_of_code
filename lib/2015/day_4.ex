7002d269f220e8b30d82861d10426bd3a2c48aaa
Mafinar Khan <mafinar@gmail.com>
2018-03-11 21:52:44 -0400

    Autoformat

--- lib/2015/day_4.ex
+++ lib/2015/day_4.ex
@@ -1,5 +1,9 @@
 defmodule AdventOfCode.Y2015.Day4 do
-  defp h(s, number), do: :crypto.hash(:md5, s <> Integer.to_string(number)) |> Base.encode16()
+  defp h(s, number),
+    do:
+      :crypto.hash(:md5, s <> Integer.to_string(number))
+      |> Base.encode16()
+
   defp satisfy(_, "0" <> "0" <> "0" <> "0" <> "0" <> _, n), do: n
   defp satisfy(s, _, n), do: lowest_number(s, n + 1)
 
