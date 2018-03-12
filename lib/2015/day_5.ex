84686d7135961203d46e7c503bdfc5c17d7afb04
Mafinar Khan <mafinar@gmail.com>
2018-03-11 23:18:20 -0400

    Year: 2015, Day: 5

--- lib/2015/day_5.ex
+++ lib/2015/day_5.ex
@@ -1,7 +1,26 @@
 defmodule AdventOfCode.Y2015.Day5 do
   use AdventOfCode.Data.InputReader, year: 2015, day: 5
 
+  defp vowel?(s), do: s in ~w/a e i o u/
+
+  def nice?(s), do: nice?(s, 0, 0, "")
+  def nice?("", vowels, dups, _) when vowels >= 3 and dups > 0, do: true
+  def nice?("", _, _, _), do: false
+
+  def nice?(<<s::bytes-size(1)>> <> rest, vowels, dups, e) do
+    cond do
+      (e <> s) in ~w/ab cd pq xy/ -> false
+      vowel?(s) and s == e -> nice?(rest, vowels + 1, dups + 1, s)
+      vowel?(s) -> nice?(rest, vowels + 1, dups, s)
+      s == e -> nice?(rest, vowels, dups + 1, s)
+      true -> nice?(rest, vowels, dups, s)
+    end
+  end
+
   def run do
     input!()
+    |> String.split("\n")
+    |> Enum.filter(&nice?/1)
+    |> length()
   end
 end
