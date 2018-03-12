eacace3e8e527942a820c2e1e404f9690866cd01
Mafinar Khan <mafinar@gmail.com>
2018-03-12 09:27:46 -0400

    Day(2015, 1) TCO

--- lib/2015/day_1.ex
+++ lib/2015/day_1.ex
@@ -1,11 +1,11 @@
 defmodule AdventOfCode.Y2015.Day1 do
   use AdventOfCode.Data.InputReader, year: 2015, day: 1
 
-  def floor(""), do: 0
-  def floor("(" <> rest), do: 1 + floor(rest)
-  def floor(")" <> rest), do: -1 + floor(rest)
+  def floor("", current_floor), do: current_floor
+  def floor("(" <> rst, current_floor), do: floor(rst, current_floor + 1)
+  def floor(")" <> rst, current_floor), do: floor(rst, current_floor - 1)
 
   def run do
-    input!() |> floor()
+    input!() |> floor(0)
   end
 end
