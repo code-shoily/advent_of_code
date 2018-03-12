b09d3dabeb59821781f8f6e961c578cdacb76508
Mafinar Khan <mafinar@gmail.com>
2018-03-11 21:50:23 -0400

    Year: 2015, Day: 4

---
+++ lib/2015/day_4.ex
@@ -0,0 +1,9 @@
+defmodule AdventOfCode.Y2015.Day4 do
+  defp h(s, number), do: :crypto.hash(:md5, s <> Integer.to_string(number)) |> Base.encode16()
+  defp satisfy(_, "0" <> "0" <> "0" <> "0" <> "0" <> _, n), do: n
+  defp satisfy(s, _, n), do: lowest_number(s, n + 1)
+
+  def lowest_number(s, number), do: satisfy(s, h(s, number), number)
+
+  def run, do: "bgvyzdsv" |> lowest_number(1)
+end
