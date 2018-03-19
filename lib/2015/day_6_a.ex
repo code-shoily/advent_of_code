a1c759ea3a7e6f772263a7d16f2408b6bb22c1a2
Mafinar Khan <mafinar@gmail.com>
2018-03-18 23:51:54 -0400

    Initializaition

--- lib/2015/day_6_a.ex
+++ lib/2015/day_6_a.ex
@@ -1,9 +1,25 @@
 defmodule AdventOfCode.Y2015.Day6a do
   use AdventOfCode.Data.InputReader, year: 2015, day: 6
 
-  def start(size), do: Agent.start(fn -> 0..(size * size - 1) |> Enum.to_list() end)
+  @size 1000
+
+  def start(), do: Agent.start(fn -> 0..(@size * @size - 1) |> Enum.to_list() end)
+
+  def find_range(s), do: s |> String.trim()
+
+  def parse_instruction("turn on" <> rest), do: {true, rest |> find_range()}
+  def parse_instruction("turn off" <> rest), do: {false, rest |> find_range()}
+  def parse_instruction("toggle" <> rest), do: {:toggle, rest |> find_range()}
+
+  def parse_line(line) do
+    line
+    |> String.replace("through", "")
+  end
 
   def run do
     input!()
+    |> parse_line()
+    |> String.split("\n")
+    |> Enum.map(&parse_instruction/1)
   end
 end
