e740f4dfd88dda6ae0311be8667cf86a437e84a8
Mafinar K <mafinar@gmail.com>
2018-10-08 12:58:04 -0400

    Solve 2016/3

---
+++ lib/2016/day_3.ex
@@ -0,0 +1,24 @@
+defmodule AdventOfCode.Y2016.Day3 do
+  use AdventOfCode.Data.InputReader, year: 2016, day: 3
+
+  def triangle?({a, b, c}) do
+    a + b > c and b + c > a and c + a > b
+  end
+
+  def parse(line) do
+    line
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  def run() do
+    input!()
+    |> String.split("\n")
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(&parse/1)
+    |> Enum.filter(&triangle?/1)
+    |> length
+  end
+end
