6a7e162a103b7be001e0ad0224b04ca2354711aa
Mafinar K <mafinar@gmail.com>
2019-05-03 21:49:45 -0400

    Solve day 1 problem 2

--- lib/2015/day_1.ex
+++ lib/2015/day_1.ex
@@ -1,11 +1,36 @@
 defmodule AdventOfCode.Y2015.Day1 do
+  @moduledoc """
+  Problem Link: https://adventofcode.com/2015/day/1
+  """
   use AdventOfCode.Data.InputReader, year: 2015, day: 1
 
-  def floor("", current_floor), do: current_floor
-  def floor("(" <> rst, current_floor), do: floor(rst, current_floor + 1)
-  def floor(")" <> rst, current_floor), do: floor(rst, current_floor - 1)
+  @spec floor(String.t(), non_neg_integer()) :: non_neg_integer()
+  defp floor("", cur), do: cur
+  defp floor("(" <> rst, cur), do: floor(rst, cur + 1)
+  defp floor(")" <> rst, cur), do: floor(rst, cur - 1)
 
-  def run do
+  @spec run_1() :: integer()
+  def run_1 do
     input!() |> floor(0)
+  end
+
+  @spec to_basement(String.t(), non_neg_integer(), integer()) :: non_neg_integer()
+  defp to_basement(_, pos, -1), do: pos
+  defp to_basement("(" <> rst, pos, cur), do: to_basement(rst, pos + 1, cur + 1)
+  defp to_basement(")" <> rst, pos, cur), do: to_basement(rst, pos + 1, cur - 1)
+
+  @spec run_2() :: non_neg_integer()
+  def run_2 do
+    input!()
+    |> to_basement(0, 0)
+  end
+
+  @spec run() ::
+          %{
+            problem_1: non_neg_integer(),
+            problem_2: non_neg_integer()
+          }
+  def run do
+    %{problem_1: run_1(), problem_2: run_2()}
   end
 end
