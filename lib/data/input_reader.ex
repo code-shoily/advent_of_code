4f750a55a94c1944e738af5414dd8ead756a3789
Mafinar K <mafinar@gmail.com>
2019-11-10 21:46:57 -0500

    Add typespec to helper

--- lib/data/input_reader.ex
+++ lib/data/input_reader.ex
@@ -2,14 +2,20 @@ defmodule AdventOfCode.Data.InputReader do
   @moduledoc """
   Reads input from file
   """
+  @type year :: integer()
+  @type day :: integer()
+
   defmacro __using__(year: year, day: day) do
     quote do
+      @spec build_path(year(), day()) :: String.t()
       defp build_path(year, day), do: "#{File.cwd!()}/lib/data/inputs/#{year}_#{day}.txt"
 
+      @spec read!(year(), day()) :: binary()
       def read!(year, day) do
         year |> build_path(day) |> File.read!()
       end
 
+      @spect input!() :: binary()
       def input!, do: read!(unquote(year), unquote(day))
     end
   end
