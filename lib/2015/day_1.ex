3733ba6a6c18256db96e68885fc7373699f66e62
Mafinar K <mafinar@gmail.com>
2019-11-11 00:11:35 -0500

    Refactor 2015, 1

--- lib/2015/day_1.ex
+++ lib/2015/day_1.ex
@@ -4,26 +4,26 @@ defmodule AdventOfCode.Y2015.Day1 do
   """
   use AdventOfCode.Data.InputReader, year: 2015, day: 1
 
-  @spec floor(String.t(), non_neg_integer()) :: non_neg_integer()
-  defp floor("", cur), do: cur
-  defp floor("(" <> rst, cur), do: floor(rst, cur + 1)
-  defp floor(")" <> rst, cur), do: floor(rst, cur - 1)
-
-  @spec run_1() :: integer()
+  @spec run_1() :: non_neg_integer()
   def run_1 do
     input!() |> floor(0)
   end
 
-  @spec to_basement(String.t(), non_neg_integer(), integer()) :: non_neg_integer()
-  defp to_basement(_, pos, -1), do: pos
-  defp to_basement("(" <> rst, pos, cur), do: to_basement(rst, pos + 1, cur + 1)
-  defp to_basement(")" <> rst, pos, cur), do: to_basement(rst, pos + 1, cur - 1)
+  @spec floor(String.t(), non_neg_integer()) :: non_neg_integer()
+  defp floor("", cur), do: cur
+  defp floor("(" <> rst, cur), do: floor(rst, cur + 1)
+  defp floor(")" <> rst, cur), do: floor(rst, cur - 1)
 
   @spec run_2() :: non_neg_integer()
   def run_2 do
     input!()
     |> to_basement(0, 0)
   end
+
+  @spec to_basement(String.t(), non_neg_integer(), integer()) :: non_neg_integer()
+  defp to_basement(_, pos, -1), do: pos
+  defp to_basement("(" <> rst, pos, cur), do: to_basement(rst, pos + 1, cur + 1)
+  defp to_basement(")" <> rst, pos, cur), do: to_basement(rst, pos + 1, cur - 1)
 
   @spec run() ::
           %{
