e395ddfaa66f666b29f4c0701e5968e0547ed836
Mafinar K <mafinar@gmail.com>
2019-12-01 11:18:07 -0500

    Year 2019, Day 1

--- lib/2016/day_2.ex
+++ lib/2016/day_2.ex
@@ -8,48 +8,63 @@ defmodule AdventOfCode.Y2016.Day2 do
 
   defp valid?(number), do: number <= 0 or number > 9
 
-  defp next(cur, "L") when rem(cur, 3) != 1, do: cur - 1
-  defp next(cur, "R") when rem(cur, 3) != 0, do: cur + 1
-  defp next(cur, "U"), do: cur - 3
-  defp next(cur, "D"), do: cur + 3
-  defp next(_, _), do: -1
+  defp next_1(cur, "L") when rem(cur, 3) != 1, do: cur - 1
+  defp next_1(cur, "R") when rem(cur, 3) != 0, do: cur + 1
+  defp next_1(cur, "U"), do: cur - 3
+  defp next_1(cur, "D"), do: cur + 3
+  defp next_1(_, _), do: -1
 
   defp parse([], res), do: res
-  defp parse([h | t], []), do: parse(t, [find(h, @initial_position)])
-  defp parse([h | t], [x | _] = res), do: parse(t, [find(h, x) | res])
+  defp parse([h | t], []), do: parse(t, [find(h, @initial_position, &next_1/2)])
+  defp parse([h | t], [x | _] = res), do: parse(t, [find(h, x, &next_1/2) | res])
 
-  defp find([], cur), do: cur
+  defp find([], cur, _), do: cur
 
-  defp find([h | t], cur) do
-    next_key = next(cur, h)
+  defp find([h | t], cur, next_fn) do
+    next_key = next_fn.(cur, h)
 
     if valid?(next_key) do
-      find(t, cur)
+      find(t, cur, next_fn)
     else
-      find(t, next_key)
+      find(t, next_key, next_fn)
     end
   end
 
-  defp to_int(value) do
-    case Integer.parse(value) do
-      {res, ""} -> res
-      _ -> :error
-    end
-  end
-
+  @spec run_1 :: :error | integer
   def run_1 do
-    input!()
-    |> String.split("\n")
-    |> Enum.map(&String.graphemes/1)
+    process_input()
     |> parse([])
     |> Enum.reverse()
     |> Enum.join()
-    |> to_int()
+    |> String.to_integer()
   end
 
-  def run_2 do
-    0
+  defp process_input do
+    input!()
+    |> String.split("\n")
+    |> Enum.map(&String.graphemes/1)
   end
+
+  # @matrix [
+  #   [nil, nil, "1", nil, nil],
+  #   [nil, "2", "3", "4", nil],
+  #   ["5", "6", "7", "8", "9"],
+  #   [nil, "10", "11", "12", nil],
+  #   [nil, nil, "13", nil, nil]
+  # ]
+  @spec run_2 :: non_neg_integer
+  def run_2 do
+    input!()
+    |> String.split("\n")
+    |> length
+  end
+
+  defp translate(10), do: "A"
+  defp translate(11), do: "B"
+  defp translate(12), do: "C"
+  defp translate(13), do: "D"
+  defp translate(n) when is_integer(n), do: to_string(n)
+  defp translate(n), do: n
 
   @spec run :: %{
           problem_1: integer(),
---
+++ lib/2019/day_1.ex
@@ -0,0 +1,48 @@
+defmodule AdventOfCode.Y2019.Day1 do
+  @moduledoc """
+  Problem Link: https://adventofcode.com/2019/day/1
+  """
+  use AdventOfCode.Data.InputReader, year: 2019, day: 1
+
+  @spec run :: %{
+          problem_1: non_neg_integer(),
+          problem_2: non_neg_integer()
+        }
+  def run do
+    %{
+      problem_1: run_1(),
+      problem_2: run_2()
+    }
+  end
+
+  @spec run_1 :: non_neg_integer()
+  def run_1 do
+    input!()
+    |> process()
+    |> Stream.map(&fuel_required/1)
+    |> Enum.sum()
+  end
+
+  @spec run_2 :: non_neg_integer()
+  def run_2 do
+    input!()
+    |> process()
+    |> Stream.map(&fuel_required/1)
+    |> Stream.map(&refuel(&1, &1))
+    |> Enum.sum()
+  end
+
+  defp process(data) do
+    data
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp fuel_required(mass), do: Integer.floor_div(mass, 3) - 2
+
+  defp refuel(fuel, total) do
+    next = fuel_required(fuel)
+    (next < 1 && total) || refuel(next, total + next)
+  end
+end
