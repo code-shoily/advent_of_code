7ed7b1db5283c8c752a4c8b8e4ce658ab001a18c
Mafinar K <mafinar@gmail.com>
2019-12-03 11:35:51 -0500

    Remove the |> String.trim from 2019_1

---
+++ lib/2019/day_3.ex
@@ -0,0 +1,6 @@
+defmodule AdventOfCode.Y2019.Day3 do
+  @moduledoc """
+  Problem description: Problem description: https://adventofcode.com/2019/day/3
+  """
+  use AdventOfCode.Data.InputReader, year: 2019, day: 3
+end
