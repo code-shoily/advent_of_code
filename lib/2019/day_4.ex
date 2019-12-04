efa61cb0ac9e9e61e3085ef5098ace8d02bbfcad
Mafinar K <mafinar@gmail.com>
2019-12-04 01:48:52 -0500

    Solve year 2019, day 4

---
+++ lib/2019/day_4.ex
@@ -0,0 +1,44 @@
+defmodule AdventOfCode.Y2019.Day4 do
+  @moduledoc """
+  Problem description: Problem description: https://adventofcode.com/2019/day/4
+  """
+  @range 245_182..790_572
+
+  def run do
+    %{
+      problem_1: run_1(),
+      problem_2: run_2()
+    }
+  end
+
+  def run_1 do
+    Enum.count(for n <- @range, n |> adjacent() and n |> increasing(), do: n)
+  end
+
+  def adjacent(number) do
+    number
+    |> Integer.digits()
+    |> Enum.dedup()
+    |> Kernel.!=(Integer.digits(number))
+  end
+
+  def increasing(number) do
+    number
+    |> Integer.digits()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [a, b] -> a <= b end)
+    |> Enum.all?()
+  end
+
+  def run_2 do
+    Enum.count(for n <- @range, n |> adjacent_2() and n |> increasing(), do: n)
+  end
+
+  def adjacent_2(number) do
+    number
+    |> Integer.digits()
+    |> Enum.chunk_by(& &1)
+    |> Enum.filter(fn dups -> Enum.count(dups) == 2 end)
+    |> Enum.any?()
+  end
+end
