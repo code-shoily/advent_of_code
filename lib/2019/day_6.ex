cc4ae3b4b0dcdf65d85ed355ef50ca5442a4b8d7
Mafinar Khan <mafinar@gmail.com>
2019-12-06 23:35:23 -0500

    Refactoring function orders 19/6

--- lib/2019/day_6.ex
+++ lib/2019/day_6.ex
@@ -4,31 +4,8 @@ defmodule AdventOfCode.Y2019.Day6 do
   """
   use AdventOfCode.Data.InputReader, year: 2019, day: 6
 
-  def process() do
-    input!() |> String.split("\n") |> Enum.map(&String.split(&1, ")"))
-  end
-
-  def to_graph(data, graph) do
-    data
-    |> Enum.each(fn [a, b] ->
-      :digraph.add_vertex(graph, a)
-      :digraph.add_vertex(graph, b)
-      :digraph.add_edge(graph, b, a)
-      :digraph.add_edge(graph, a, b)
-    end)
-
-    graph
-  end
-
-  def count_orbits(graph) do
-    graph
-    |> :digraph.vertices()
-    |> Stream.map(fn vertex ->
-      :digraph.get_path(graph, vertex, "COM") || []
-    end)
-    |> Stream.map(fn path -> length(path) - 1 end)
-    |> Enum.sum()
-    |> Kernel.+(1)
+  def run do
+    %{problem_1: run_1(), problem_2: run_2()}
   end
 
   def run_1 do
@@ -43,14 +20,37 @@ defmodule AdventOfCode.Y2019.Day6 do
     |> count_orbital_transfers()
   end
 
-  def count_orbital_transfers(graph) do
+  def process() do
+    input!() |> String.split("\n") |> Enum.map(&String.split(&1, ")"))
+  end
+
+  defp to_graph(data, graph) do
+    data
+    |> Enum.each(fn [a, b] ->
+      :digraph.add_vertex(graph, a)
+      :digraph.add_vertex(graph, b)
+      :digraph.add_edge(graph, b, a)
+      :digraph.add_edge(graph, a, b)
+    end)
+
+    graph
+  end
+
+  defp count_orbits(graph) do
+    graph
+    |> :digraph.vertices()
+    |> Stream.map(fn vertex ->
+      :digraph.get_path(graph, vertex, "COM") || []
+    end)
+    |> Stream.map(fn path -> length(path) - 1 end)
+    |> Enum.sum()
+    |> Kernel.+(1)
+  end
+
+  defp count_orbital_transfers(graph) do
     graph
     |> :digraph.get_short_path("YOU", "SAN")
     |> Enum.count()
     |> Kernel.-(3)
-  end
-
-  def run do
-    %{problem_1: run_1(), problem_2: run_2()}
   end
 end
