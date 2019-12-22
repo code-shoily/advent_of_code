5d529f3487a585827c03d4db17a6fdce6195c968
Mafinar Khan <mafinar@gmail.com>
2019-12-22 13:31:15 -0500

    Remove unnecessary file

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,18 +1,3 @@
 defmodule AdventOfCode do
-  @moduledoc """
-  Documentation for AdventOfCode.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AdventOfCode.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -1,9 +1,5 @@
 defmodule AdventOfCodeTest do
   @moduledoc false
   use ExUnit.Case
-  doctest AdventOfCode
-
-  test "greets the world" do
-    assert AdventOfCode.hello() == :world
-  end
+  doctest AdventOfCode.Helpers.InputParser
 end
