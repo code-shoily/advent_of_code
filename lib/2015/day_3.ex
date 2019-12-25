93a0710abcb302e6d3c176b51d0674e03bd7e2fc
Mafinar Khan <mafinar@gmail.com>
2019-12-25 17:13:13 -0500

    Solve Year 2015, Problem 3, Part 2

--- lib/2015/day_3.ex
+++ lib/2015/day_3.ex
@@ -3,6 +3,12 @@ defmodule AdventOfCode.Y2015.Day3 do
   Problem Link: https://adventofcode.com/2015/day/3
   """
   use AdventOfCode.Data.InputReader, year: 2015, day: 3
+  def run, do: {run_1(), run_2()}
+
+  def run_1 do
+    input!() |> deliver_present() |> MapSet.size()
+  end
+
   @start [0, 0]
 
   defp next_location("^", [x, y]), do: [x, y + 1]
@@ -10,25 +16,37 @@ defmodule AdventOfCode.Y2015.Day3 do
   defp next_location(">", [x, y]), do: [x + 1, y]
   defp next_location("<", [x, y]), do: [x - 1, y]
 
-  def deliver_present(<<direction::bytes-size(1)>> <> rest) do
+  defp deliver_present(<<direction::bytes-size(1)>> <> rest) do
     next = next_location(direction, @start)
     deliver_present(rest, next, MapSet.new([[0, 0], next]))
   end
 
-  def deliver_present("", _, db), do: db
+  defp deliver_present("", _, db), do: db
 
-  def deliver_present(<<direction::bytes-size(1)>> <> rest, current, db) do
+  defp deliver_present(<<direction::bytes-size(1)>> <> rest, current, db) do
     next = next_location(direction, current)
     deliver_present(rest, next, MapSet.put(db, next))
   end
 
-  def run_1 do
-    input!() |> deliver_present() |> MapSet.size()
-  end
-
   def run_2 do
-    0
+    input!()
+    |> String.graphemes()
+    |> alternate()
+    |> deliver_present_each()
+    |> MapSet.size()
   end
 
-  def run, do: {run_1(), run_2()}
+  defp alternate(directions) do
+    santa = directions |> Enum.take_every(2) |> Enum.join()
+    robo_santa = directions |> tl() |> Enum.take_every(2) |> Enum.join()
+
+    {santa, robo_santa}
+  end
+
+  defp deliver_present_each({santa, robo_santa}) do
+    MapSet.union(
+      deliver_present(santa),
+      deliver_present(robo_santa)
+    )
+  end
 end
