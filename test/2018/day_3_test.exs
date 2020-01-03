c68cbe19d0986cab2bd4c02b9e09fafb81805030
Mafinar Khan <mafinar@gmail.com>
2020-01-03 17:01:50 -0500

    Solve Year 2018, Day 3

--- test/2018/day_3_test.exs
+++ test/2018/day_3_test.exs
@@ -1,8 +1,25 @@
 defmodule AdventOfCode.Y2018.Day3Test do
   @moduledoc false
   use ExUnit.Case
+  @moduletag :y183
 
   alias AdventOfCode.Y2018.Day3, as: Solution
+
+  test "Input processor" do
+    sample = """
+    #1 @ 1,3: 4x4
+    #2 @ 3,1: 4x4
+    #3 @ 5,5: 2x2
+    """
+
+    output = [
+      %{id: 1, dimension: {4, 4}, origin: {1, 3}},
+      %{id: 2, dimension: {4, 4}, origin: {3, 1}},
+      %{id: 3, dimension: {2, 2}, origin: {5, 5}}
+    ]
+
+    assert Solution.process(sample) == output
+  end
 
   test "Year 2018, Day 3, Part 1" do
     assert Solution.run_1() == 110_389
