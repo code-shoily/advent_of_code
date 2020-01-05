c050d01ba5bd6c596ba0460279a453de9191eb1e
Mafinar Khan <mafinar@gmail.com>
2020-01-04 20:35:12 -0500

    Solve Year 2018, Day 5

--- test/2018/day_5_test.exs
+++ test/2018/day_5_test.exs
@@ -1,14 +1,19 @@
 defmodule AdventOfCode.Y2018.Day5Test do
   @moduledoc false
   use ExUnit.Case
+  @moduletag :y185
 
   alias AdventOfCode.Y2018.Day5, as: Solution
 
+  test "React (1)" do
+    assert Solution.react('dabAcCaCBAcCcaDA') == 10
+  end
+
   test "Year 2018, Day 5, Part 1" do
-    assert Solution.run_1() == 4284
+    assert Solution.run_1() == 10496
   end
 
   test "Year 2018, Day 5, Part 2" do
-    assert Solution.run_2() == 35490
+    assert Solution.run_2() == 5774
   end
 end
