7d05af62adcc4355f4ad06e35f7752a09bf64216
Mafinar K <mafinar@gmail.com>
2020-07-02 23:38:26 -0400

    Add boilerplate for problem

---
+++ lib/2016/day_4.ex
@@ -0,0 +1,20 @@
+defmodule AdventOfCode.Y2016.Day4 do
+  @moduledoc """
+  Problem Link: https://adventofcode.com/2016/day/4
+  """
+  use AdventOfCode.Data.InputReader, year: 2016, day: 4
+
+  def process(input) do
+    input
+  end
+
+  def run_1 do
+    {:not_implemented, 1}
+  end
+
+  def run_2 do
+    {:not_implemented, 2}
+  end
+
+  def run, do: {run_1(), run_2()}
+end
---
+++ test/2016/day_4_test.exs
@@ -0,0 +1,15 @@
+defmodule AdventOfCode.Y2016.Day4Test do
+  @moduledoc false
+  use ExUnit.Case
+  @moduletag :y20164
+
+  alias AdventOfCode.Y16.Day4, as: Solution
+
+  test "Year 2016, Day 4, Part 1" do
+    assert Solution.run_1() == nil
+  end
+
+  test "Year 2016, Day 4, Part 2" do
+    assert Solution.run_2() == nil
+  end
+end
