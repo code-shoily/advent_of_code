421a16f58f9d947a72517f59e4b543614475cc5a
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-11-07 22:29:08 -0500

    Solve year 2018 day 6 part 1

--- lib/2018/day_6.ex
+++ lib/2018/day_6.ex
@@ -4,12 +4,90 @@ defmodule AdventOfCode.Y2018.Day6 do
   """
   use AdventOfCode.Data.InputReader, year: 2018, day: 6
 
-  def process do
-    input!()
+  @type point :: {integer(), integer()}
+  @type corners :: {point, point}
+
+  def process(data \\ input!()) do
+    data
+    |> String.split("\n")
+    |> Enum.map(
+      &(&1
+        |> String.split(",")
+        |> Enum.map(fn n -> n |> String.trim() |> String.to_integer() end)
+        |> List.to_tuple())
+    )
+  end
+
+  def manhattan_distance({x1, y1}, {x2, y2}), do: abs(x2 - x1) + abs(y2 - y1)
+
+  @spec get_corners([point]) :: corners
+  def get_corners(points) do
+    {{xmin, _}, {xmax, _}} = Enum.min_max_by(points, &elem(&1, 0))
+    {{_, ymin}, {_, ymax}} = Enum.min_max_by(points, &elem(&1, 1))
+
+    {{xmin, ymin}, {xmax, ymax}}
+  end
+
+  @spec create_world(corners) :: %{required(point) => integer()}
+  def create_world({{xl, yt}, {xr, yb}}) do
+    for x <- xl..xr, y <- yt..yb, into: %{} do
+      {{x, y}, 0}
+    end
+  end
+
+  def remove_edges(points, {{xl, yt}, {xr, yb}}) do
+    extremes = MapSet.new([xl, xr, yt, yb])
+
+    points
+    |> Enum.reject(fn {x, y} ->
+      x in extremes or y in extremes
+    end)
+  end
+
+  defp label_points(points) do
+    points
+    |> Enum.with_index(1)
+    |> Enum.into(%{})
+  end
+
+  defp create_cache(labeled_points) do
+    labeled_points
+    |> Map.values()
+    |> Enum.map(&{&1, 0})
+    |> Enum.into(%{})
+  end
+
+  defp initialize_universe() do
+    points = process()
+    corners = get_corners(points)
+    world = create_world(corners)
+
+    finite_points = remove_edges(points, corners)
+    labels = label_points(finite_points)
+    cache = create_cache(labels)
+
+    %{
+      points: finite_points,
+      world: world,
+      labels: labels,
+      cache: cache
+    }
+  end
+
+  def get_nearest_area(areas) do
+    areas
+    |> Enum.sort_by(fn {_, distance} -> distance end)
+    |> Enum.take(2)
+    |> (fn
+          [{_, d}, {_, d}] -> 0
+          [{p, _} | _] -> p
+        end).()
   end
 
   def run_1 do
-    {:not_implemented, 1}
+    %{points: _, world: _, labels: _, cache: cache} = initialize_universe()
+
+    Enum.max_by(cache, fn {_, v} -> v end)
   end
 
   def run_2 do
--- test/2018/day_6_test.exs
+++ test/2018/day_6_test.exs
@@ -1,14 +1,89 @@
 defmodule AdventOfCode.Y2018.Day6Test do
   @moduledoc false
   use ExUnit.Case
+  @moduletag :y186
 
   alias AdventOfCode.Y2018.Day6, as: Solution
 
-  test "Year 2018, Day 6, Part 1" do
-    assert Solution.run_1() == nil
+  @data "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9"
+
+  @points [
+    {1, 1},
+    {1, 6},
+    {8, 3},
+    {3, 4},
+    {5, 5},
+    {8, 9}
+  ]
+
+  describe "process" do
+    test "Processes input by creating a list of points from a set of strings" do
+      assert Solution.process(@data) == @points
+    end
   end
 
-  test "Year 2018, Day 6, Part 2" do
-    assert Solution.run_2() == nil
+  describe "manhattan_distance" do
+    test "Computes manhattan distance between origin and another" do
+      assert Solution.manhattan_distance({0, 0}, {10, 20}) == 30
+    end
+
+    test "Computes manhattan distance when there is no negative" do
+      assert Solution.manhattan_distance({100, 50}, {10, 10}) == 130
+    end
+
+    test "Computes manhattan distance when there is no positive" do
+      assert Solution.manhattan_distance({15, 17}, {16, 19}) == 3
+    end
   end
+
+  describe "get_corners/1" do
+    test "Computes the extreme corners of a set of points" do
+      assert Solution.get_corners(@points) == {{1, 1}, {8, 9}}
+    end
+  end
+
+  describe "create_world/1" do
+    test "Valid world is generated" do
+      corners = Solution.get_corners(@points)
+      world = Solution.create_world(corners)
+      keys = Map.keys(world)
+
+      assert length(keys) == 72
+
+      assert keys |> Enum.map(&elem(&1, 0)) |> Enum.uniq() |> Enum.sort() ==
+               Enum.into(1..8, [])
+
+      assert keys |> Enum.map(&elem(&1, 1)) |> Enum.uniq() |> Enum.sort() ==
+               Enum.into(1..9, [])
+
+      assert world |> Map.values() |> Enum.sum() == 0
+    end
+  end
+
+  describe "remove_edges/1" do
+    test "edges are removed from the points" do
+      corners = Solution.get_corners(@points)
+      assert Solution.remove_edges(@points, corners) == [{3, 4}, {5, 5}]
+    end
+  end
+
+  describe "nearest_area/1" do
+    test "Finds the nearest area" do
+      areas = %{7 => 10, 1 => 11, 2 => 15}
+      assert Solution.get_nearest_area(areas) == 7
+    end
+
+    test "Returns 0 for tied nearest area" do
+      areas = %{0 => 10, 1 => 10, 2 => 15}
+      assert Solution.get_nearest_area(areas) == 0
+    end
+  end
+
+  # test "Year 2018, Day 6, Part 1" do
+  #   assert Solution.run_1() == nil
+  # end
+
+  # test "Year 2018, Day 6, Part 2" do
+  #   assert Solution.run_2() == nil
+  # end
 end
