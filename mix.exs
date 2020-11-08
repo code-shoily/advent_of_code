843afdcda3f442683e00d6053bb0631a9c91e10d
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-11-07 20:36:09 -0500

    Upgrade things

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCode.MixProject do
     [
       app: :advent_of_code,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,7 +21,7 @@ defmodule AdventOfCode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:jason, "~> 1.2"}
     ]
   end
