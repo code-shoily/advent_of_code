d8493c757d8939d58073b943cfa3b372f791271d
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-12-04 09:53:57 -0500

    Refactor field checker

--- lib/2020/day_4.ex
+++ lib/2020/day_4.ex
@@ -4,12 +4,8 @@ defmodule AdventOfCode.Y2020.Day4 do
   """
   use AdventOfCode.Helpers.InputReader, year: 2020, day: 4
 
-  def run_1,
-    do: input!() |> process() |> Enum.filter(&ok?(:fields, &1)) |> length()
-
-  def run_2,
-    do: input!() |> process() |> Enum.filter(&ok?(:values, &1)) |> length()
-
+  def run_1, do: input!() |> process() |> Enum.filter(&ok?(:k, &1)) |> length()
+  def run_2, do: input!() |> process() |> Enum.filter(&ok?(:v, &1)) |> length()
   def run, do: {run_1(), run_2()}
 
   def process(input) do
@@ -17,70 +13,52 @@ defmodule AdventOfCode.Y2020.Day4 do
     |> String.split("\n")
     |> Enum.chunk_by(&(&1 == ""))
     |> Enum.reject(&(&1 == [""]))
-    |> Enum.map(&as_map/1)
+    |> Enum.map(&as_pp/1)
   end
 
-  defp as_map(list) do
-    list
+  defp as_pp(fields) do
+    fields
     |> Enum.join(" ")
     |> String.split(" ")
     |> Enum.map(&List.to_tuple(String.split(&1, ":")))
     |> Enum.into(%{})
+    |> Map.put_new("cid", nil)
+    |> cast()
   end
 
-  @required_fields ~w/byr cid ecl eyr hcl hgt iyr pid/
-  defp ok?(:fields, passport) do
-    fields = MapSet.new(Map.keys(passport))
-    required = MapSet.new(@required_fields)
-    missing = MapSet.to_list(MapSet.difference(required, fields))
+  @fields ~w/byr cid ecl eyr hcl hgt iyr pid/
 
-    case missing do
-      [] -> true
-      ["cid"] -> true
-      _ -> false
-    end
-  end
+  @birth_year 1920..2002
+  @expiration_year 2020..2030
+  @eye_colors ~w/amb blu brn gry grn hzl oth/
+  @height_cm 59..76
+  @height_inch 150..193
+  @issuing_year 2010..2020
 
-  defp ok?(:values, passport) do
-    @required_fields
-    |> Enum.map(&ok?(&1, passport))
-    |> Enum.all?()
-  end
+  defp ok?(:k, pp), do: map_size(Map.take(pp, @fields)) == 8
+  defp ok?(:v, pp), do: Enum.all?(Enum.map(@fields, &ok?(&1, pp)))
 
-  defp ok?("byr", %{"byr" => byr}) do
-    byr = String.to_integer(byr)
-    1920 <= byr and byr <= 2002
-  end
-
-  defp ok?("eyr", %{"eyr" => eyr}) do
-    eyr = String.to_integer(eyr)
-    2020 <= eyr and eyr <= 2030
-  end
-
-  defp ok?("iyr", %{"iyr" => iyr}) do
-    iyr = String.to_integer(iyr)
-    2010 <= iyr and iyr <= 2020
-  end
-
+  defp ok?("cid", %{}), do: true
+  defp ok?("byr", %{"byr" => byr}), do: byr in @birth_year
+  defp ok?("ecl", %{"ecl" => ecl}), do: ecl in @eye_colors
+  defp ok?("eyr", %{"eyr" => eyr}), do: eyr in @expiration_year
+  defp ok?("hcl", %{"hcl" => "#" <> hcl}), do: String.length(hcl) == 6
+  defp ok?("hgt", %{"hgt" => {hgt, "cm"}}) when hgt in @height_inch, do: true
+  defp ok?("hgt", %{"hgt" => {hgt, "in"}}) when hgt in @height_cm, do: true
+  defp ok?("hgt", %{"hgt" => _}), do: false
+  defp ok?("iyr", %{"iyr" => iyr}), do: iyr in @issuing_year
   defp ok?("pid", %{"pid" => pid}), do: String.length(pid) == 9 && String.to_integer(pid)
-  defp ok?("ecl", %{"ecl" => ecl}), do: ecl in ~w/amb blu brn gry grn hzl oth/
-
-  defp ok?("hcl", %{"hcl" => hcl}) do
-    case String.graphemes(hcl) do
-      ["#" | digits] -> length(digits) == 6
-      _ -> false
-    end
-  end
-
-  defp ok?("hgt", %{"hgt" => hgt}) do
-    case Integer.parse(hgt) do
-      {height, "cm"} when 150 <= height and height <= 193 -> true
-      {height, "in"} when 59 <= height and height <= 76 -> true
-      _ -> false
-    end
-  end
-
-  defp ok?("cid", _), do: true
 
   defp ok?(_, _), do: false
+
+  defp cast(%{"byr" => b, "eyr" => e, "hgt" => h, "iyr" => i} = pp) do
+    Map.merge(pp, %{
+      "byr" => String.to_integer(b),
+      "eyr" => String.to_integer(e),
+      "hgt" => Integer.parse(h),
+      "iyr" => String.to_integer(i)
+    })
+  end
+
+  defp cast(pp), do: pp
 end
