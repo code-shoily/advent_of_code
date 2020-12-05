4828179a8534bbf4025b6f609a877c4f6befb992
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-12-05 00:57:12 -0500

    Solve year 2020 day 5

--- test/2020/day_5_test.exs
+++ test/2020/day_5_test.exs
@@ -7,10 +7,10 @@ defmodule AdventOfCode.Y2020.Day5Test do
   alias AdventOfCode.Y2020.Day5, as: Solution
 
   test "Year 2020, Day 5, Part 1" do
-    assert Solution.run_1() == nil
+    assert Solution.run_1() == 930
   end
 
   test "Year 2020, Day 5, Part 2" do
-    assert Solution.run_2() == nil
+    assert Solution.run_2() == 515
   end
 end
