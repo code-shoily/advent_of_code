2ec9f8a302e8e989f6ab362786f589f6095e18dc
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-12-10 21:38:54 -0500

    Solve year 2020 day 10 part 2

--- lib/2020/day_10.ex
+++ lib/2020/day_10.ex
@@ -5,14 +5,16 @@ defmodule AdventOfCode.Y2020.Day10 do
   use AdventOfCode.Helpers.InputReader, year: 2020, day: 10
 
   def run_1, do: input!() |> process() |> rates() |> multiply_1_3()
-  def run_2, do: {:not_implemented, 2}
+  def run_2, do: input!() |> process() |> count()
+
   def run, do: {run_1(), run_2()}
 
   def process(input),
-    do: input |> String.split("\n") |> Enum.map(&String.to_integer/1) |> MapSet.new()
+    do: [0 | Enum.map(String.split(input, "\n"), &String.to_integer/1)] |> Enum.sort(:desc)
 
   defp rates(data) do
     my_rate = Enum.max(data) + 3
+    data = MapSet.new(data)
 
     Stream.unfold(0, fn
       n when n > my_rate ->
@@ -25,11 +27,16 @@ defmodule AdventOfCode.Y2020.Day10 do
     end)
   end
 
-  defp diffs(a, b) when a - b == 1, do: {{1, 0}, a}
-  defp diffs(a, b) when a - b == 2, do: {{0, 0}, a}
-  defp diffs(a, b) when a - b == 3, do: {{0, 1}, a}
+  defp diffs(a, b) when a - b == 1, do: {{a, 1, 0}, a}
+  defp diffs(a, b) when a - b == 3, do: {{a, 0, 1}, a}
   defp diffs(_, _), do: nil
 
   defp multiply_1_3(r),
-    do: apply(&Kernel.*/2, Enum.reduce(r, [0, 0], fn {a, b}, [x, y] -> [x + a, y + b] end))
+    do: apply(&Kernel.*/2, Enum.reduce(r, [0, 0], fn {_, a, b}, [x, y] -> [x + a, y + b] end))
+
+  defp count(data), do: count(data, %{(hd(data) + 3) => 1})
+  defp count([], cache), do: cache[0]
+
+  defp count([h | rst], m),
+    do: count(rst, Map.put(m, h, Enum.sum(Enum.map(1..3, &Map.get(m, h + &1, 0)))))
 end
--- test/2020/day_10_test.exs
+++ test/2020/day_10_test.exs
@@ -7,10 +7,10 @@ defmodule AdventOfCode.Y2020.Day10Test do
   alias AdventOfCode.Y2020.Day10, as: Solution
 
   test "Year 2020, Day 10, Part 1" do
-    assert Solution.run_1() == nil
+    assert Solution.run_1() == 2030
   end
 
   test "Year 2020, Day 10, Part 2" do
-    assert Solution.run_2() == nil
+    assert Solution.run_2() == 42_313_823_813_632
   end
 end
