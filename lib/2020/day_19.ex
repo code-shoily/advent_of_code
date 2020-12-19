945f55cc5c008e9d6a97a9211403827699822fd9
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-12-19 16:00:44 -0500

    Solve year 2020 day 19, Part: 2

--- lib/2020/day_19.ex
+++ lib/2020/day_19.ex
@@ -1,41 +1,40 @@
 defmodule AdventOfCode.Y2020.Day19 do
-  @moduledoc """
-  Problem Link: https://adventofcode.com/2020/day/19
-  """
   use AdventOfCode.Helpers.InputReader, year: 2020, day: 19
 
-  @alphabet ~w/a b |/
+  def run_1, do: match_count(process(input!(), false))
+  def run_2, do: match_count(process(input!(), true))
 
-  def run_1 do
-    {rules, messages} = process(input!())
-
-    regex = to_regex(:erlang.iolist_to_binary(rule_to_regex(rules, "0", "")))
-
-    length(Enum.filter(messages, &Regex.match?(regex, &1)))
+  def process(input, loop?) do
+    {rules, messages} = input |> String.split("\n\n") |> parse() |> maybe_override(loop?)
+    {messages, rules |> to_re("0", "", loop?) |> :erlang.iolist_to_binary() |> to_re()}
   end
 
-  def process(input), do: parse(String.split(input, "\n\n"))
+  defp maybe_override(parsed, loop?),
+    do: (loop? && override_8_11(parsed)) || Function.identity(parsed)
 
+  @overrides %{"8" => "42 +", "11" => "42 31 | 42 11 31"}
+  def override_8_11({rules, messages}), do: {Map.merge(rules, @overrides), messages}
   def parse([rules, messages]), do: {parse_rules(rules), parse_messages(messages)}
-
-  defp parse_rules(rules) do
-    rules
-    |> String.split("\n")
-    |> Enum.map(&String.replace(&1, "\"", ""))
-    |> Enum.map(fn rule ->
-      [idx, rule] = String.split(rule, ":")
-      {idx, String.trim(rule)}
-    end)
-    |> Enum.into(%{})
-  end
-
   defp parse_messages(messages), do: String.split(messages, "\n")
 
-  defp rule_to_regex(rules, idx, regex) do
-    rules[idx]
-    |> String.split(" ")
-    |> Enum.map(&((&1 in @alphabet && &1) || ["(", rule_to_regex(rules, &1, regex), ")"]))
+  defp parse_rules(rules) do
+    for rule <- String.split(rules, "\n"), into: %{} do
+      [idx, rule] = String.split(String.replace(rule, "\"", ""), ":")
+      {idx, String.trim(rule)}
+    end
   end
 
-  defp to_regex(source), do: %Regex{source: "^" <> source <> "$"}
+  defp to_re(rules, "11", re, true),
+    do: ["(?<m>(#{to_re(rules, "42", re, true)})(?&m)?(#{to_re(rules, "31", re, true)}))"]
+
+  @reserved ~w/a b | +/
+  defp to_re(rules, idx, re, loop?) do
+    rules[idx]
+    |> String.split(" ")
+    |> Enum.map(&((&1 in @reserved && &1) || ["(", to_re(rules, &1, re, loop?), ")"]))
+  end
+
+  defp to_re(source), do: %Regex{source: splat("^" <> source <> "$")}
+  defp splat(re), do: Enum.reduce(@reserved, re, &String.replace(&2, "(#{&1})", &1))
+  defp match_count({messages, re}), do: length(Enum.filter(messages, &Regex.match?(re, &1)))
 end
--- test/2020/day_19_test.exs
+++ test/2020/day_19_test.exs
@@ -11,6 +11,6 @@ defmodule AdventOfCode.Y2020.Day19Test do
   end
 
   test "Year 2020, Day 19, Part 2" do
-    assert Solution.run_2() == nil
+    assert Solution.run_2() == 381
   end
 end
