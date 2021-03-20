225c3acaa7de04e7823654933b4f2c1dcb364d80
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-03-19 23:54:26 -0400

    Solve year 2020 day 25

---
+++ test/2020/day_25_test.exs
@@ -0,0 +1,12 @@
+defmodule AdventOfCode.Y2020.Day25Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y2025
+
+  alias AdventOfCode.Y2020.Day25, as: Solution
+
+  test "Year 2020, Day 25" do
+    assert Solution.run() == 3_286_137
+  end
+end
