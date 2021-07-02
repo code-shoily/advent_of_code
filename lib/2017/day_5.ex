3c39290b9b00c41cffb497c5321bad66c44f8524
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-07-02 18:20:25 -0400

    Refactor

--- lib/2017/day_5.ex
+++ lib/2017/day_5.ex
@@ -4,24 +4,24 @@ defmodule AdventOfCode.Y2017.Day5 do
   """
   use AdventOfCode.Helpers.InputReader, year: 2017, day: 5
 
-  def run_1, do: process() |> jump_1(0, 0)
-  def run_2, do: process() |> jump_2(0, 0)
+  def run_1, do: process() |> then(fn _ -> jump_1(0, 0) end)
+  def run_2, do: process() |> then(fn _ -> jump_2(0, 0) end)
   def run, do: {run_1(), run_2()}
 
   def process(input \\ input!()) do
-    input |> String.split("\n") |> Enum.map(&String.to_integer/1) |> as_map()
+    input
+    |> String.split("\n")
+    |> Enum.with_index()
+    |> Enum.map(fn {v, k} -> Process.put(k, String.to_integer(v)) end)
   end
 
-  def as_map(instructions) do
-    instructions |> Enum.with_index() |> Enum.map(&{elem(&1, 1), elem(&1, 0)}) |> Enum.into(%{})
-  end
+  def jump_1(x, steps), do: jump_1(Process.get(x), x, steps)
+  def jump_1(i, _, steps) when is_nil(i), do: steps
+  def jump_1(i, x, steps), do: Process.put(x, i + 1) |> then(fn _ -> jump_1(x + i, steps + 1) end)
 
-  def jump_1(map, x, steps), do: jump_1(map, map[x], x, steps)
-  def jump_1(_, i, _, steps) when is_nil(i), do: steps
-  def jump_1(map, i, x, steps), do: jump_1(Map.put(map, x, i + 1), x + i, steps + 1)
+  def jump_2(x, steps), do: jump_2(Process.get(x), x, steps)
+  def jump_2(i, _, steps) when is_nil(i), do: steps
 
-  def jump_2(map, x, steps), do: jump_2(map, map[x], x, steps)
-  def jump_2(_, i, _, steps) when is_nil(i), do: steps
-  def jump_2(map, i, x, steps) when i > 2, do: jump_2(Map.put(map, x, i - 1), x + i, steps + 1)
-  def jump_2(map, i, x, steps), do: jump_2(Map.put(map, x, i + 1), x + i, steps + 1)
+  def jump_2(i, x, steps),
+    do: Process.put(x, (i > 2 && i - 1) || i + 1) |> then(fn _ -> jump_2(x + i, steps + 1) end)
 end
