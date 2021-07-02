54e2f35338daf9f98469e06ce6086670cf5956d8
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-07-02 09:02:49 -0400

    Solve Year 2017, Day 5

---
+++ test/2017/day_5_test.exs
@@ -0,0 +1,16 @@
+defmodule AdventOfCode.Y2017.Day5Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y175
+
+  alias AdventOfCode.Y2017.Day5, as: Solution
+
+  test "Year 2017, Day 5, Part 1" do
+    assert Solution.run_1() == 372_671
+  end
+
+  test "Year 2017, Day 5, Part 2" do
+    assert Solution.run_2() == 25_608_480
+  end
+end
