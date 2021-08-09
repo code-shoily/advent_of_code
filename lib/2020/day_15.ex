8f99ecdfdfd642b6dcaf7f0812942dedb82db96b
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-08-09 01:53:00 -0400

    Remove slow implementtion of 20-15

--- lib/2020/day_15.ex
+++ lib/2020/day_15.ex
@@ -2,19 +2,26 @@ defmodule AdventOfCode.Y2020.Day15 do
   @moduledoc """
   Problem Link: https://adventofcode.com/2020/day/15
   """
-  def run_1, do: [6, 19, 0, 5, 7, 13, 1] |> prepare() |> speak(2020)
-  def run_2, do: [6, 19, 0, 5, 7, 13, 1] |> prepare() |> speak(30_000_000)
+  def run_1, do: [6, 19, 0, 5, 7, 13, 1] |> play(2020)
+  def run_2, do: [6, 19, 0, 5, 7, 13, 1] |> play(30_000_000)
 
-  def prepare(dat) do
-    {List.last(dat), for({v, i} <- Enum.with_index(dat, 1), into: %{}, do: {v, [i]}), length(dat)}
+  def play(input, stop) do
+    tab = :ets.new(:tab, [])
+    true = :ets.insert(tab, Enum.with_index(input, 1))
+    speak({List.last(input), tab, length(input)}, stop)
   end
 
-  defp speak({val, _, stop}, stop), do: val
+  defp speak({last, _hist, stop}, stop), do: last
 
-  defp speak({val, mem, turn}, stop) do
-    case {Map.get(mem, val), turn + 1} do
-      {[_], t} -> speak({0, Map.update(mem, 0, [t], &[t | &1]), t}, stop)
-      {[a, b | _], t} -> speak({a - b, Map.update(mem, a - b, [t], &[t | &1]), t}, stop)
+  defp speak({last, hist, turn}, stop) do
+    case :ets.lookup(hist, last) do
+      [] ->
+        true = :ets.insert(hist, {last, turn})
+        speak({0, hist, turn + 1}, stop)
+
+      [{_, prev_turn}] ->
+        true = :ets.insert(hist, {last, turn})
+        speak({turn - prev_turn, hist, turn + 1}, stop)
     end
   end
 end
--- test/2020/day_15_test.exs
+++ test/2020/day_15_test.exs
@@ -6,11 +6,11 @@ defmodule AdventOfCode.Y2020.Day15Test do
 
   alias AdventOfCode.Y2020.Day15, as: Solution
 
-  test "Year 2020, Day 15, Part 1" do
+  test "Year 2020, Day 15, Part 1: Process" do
     assert Solution.run_1() == 468
   end
 
-  test "Year 2020, Day 15, Part 2" do
+  test "Year 2020, Day 15, Part 2: Process" do
     assert Solution.run_2() == 1_801_753
   end
 end
