1fd076da2cd6ca3cf965a886aeb32878c3031c6a
Mafinar Khan <mafinar@gmail.com>
2021-10-29 07:43:26 -0400

    Refactor 2020/24

--- lib/2020/day_24.ex
+++ lib/2020/day_24.ex
@@ -1,21 +1,46 @@
 defmodule AdventOfCode.Y2020.Day24 do
   @moduledoc """
+  --- Day 24: Lobby Layout ---
   Problem Link: https://adventofcode.com/2020/day/24
   """
   use AdventOfCode.Helpers.InputReader, year: 2020, day: 24
+  require Integer
 
-  def run_1, do: input!() |> process() |> walk() |> colorify() |> Enum.count(&(not &1))
-  def run_2, do: {:not_implemented, 2}
-  def process(input), do: Enum.map(String.split(input, "\n"), &parse/1)
+  def run_1 do
+    input()
+    |> process()
+    |> flip()
+    |> black_tiles()
+    |> length()
+  end
+
+  def run_2 do
+    {:not_implemented, 2}
+  end
+
+  @directions ~r/e|w|se|sw|ne|nw/
+  def process(input) do
+    for line <- String.split(input, "\n") do
+      @directions
+      |> Regex.scan(line, capture: :first)
+      |> List.flatten()
+    end
+  end
 
   defp walk("w", {x, y}), do: {x - 1, y}
   defp walk("e", {x, y}), do: {x + 1, y}
-  defp walk("nw", {x, y}), do: {x, y - 1}
-  defp walk("ne", {x, y}), do: {x + 1, y - 1}
-  defp walk("sw", {x, y}), do: {x - 1, y + 1}
-  defp walk("se", {x, y}), do: {x, y + 1}
-  defp walk(tiles), do: Enum.map(tiles, fn sides -> Enum.reduce(sides, {0, 0}, &walk/2) end)
+  defp walk("nw", {x, y}), do: {Integer.is_even(y) && x || x - 1, y - 1}
+  defp walk("ne", {x, y}), do: {Integer.is_even(y) && x + 1 || x, y - 1}
+  defp walk("sw", {x, y}), do: {Integer.is_even(y) && x || x - 1, y + 1}
+  defp walk("se", {x, y}), do: {Integer.is_even(y) && x + 1 || x, y + 1}
 
-  defp parse(s), do: ~r/e|w|se|sw|ne|nw/ |> Regex.scan(s, capture: :first) |> List.flatten()
-  defp colorify(tiles), do: Enum.map(Enum.frequencies(tiles), &(rem(elem(&1, 1), 2) == 0))
+  defp flip(tiles), do: Enum.map(tiles, fn sides -> Enum.reduce(sides, {0, 0}, &walk/2) end)
+
+  defp black_tiles(tiles) do
+    for {_, tile} <- Enum.frequencies(tiles), rem(tile, 2) == 1, do: tile
+  end
+
+  defp input do
+    input!()
+  end
 end
