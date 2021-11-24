a5916558bcc8e7feb0dc38b1f4057dbad2ce3cc4
Mafinar Khan <mafinar@gmail.com>
2021-11-24 18:41:34 -0500

    wtf I didn't have credo all along?

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule AdventOfCode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:httpoison, "~> 1.8.0"},
       {:floki, "~> 0.32.0"},
       {:jason, "~> 1.2"}
