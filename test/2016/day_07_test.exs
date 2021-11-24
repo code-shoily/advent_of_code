d94bfaa392769e532734d6eb9a3224f01febd4c6
Mafinar Khan <mafinar@gmail.com>
2021-11-24 18:21:43 -0500

    Remove the weird p1 p2 tags

--- test/2016/day_07_test.exs
+++ test/2016/day_07_test.exs
@@ -6,12 +6,10 @@ defmodule AdventOfCode.Y2016.Day07Test do
 
   alias AdventOfCode.Y2016.Day07, as: Solution
 
-  @tag :p1
   test "Year 2016, Day 7, Part 1" do
     assert Solution.run_1() == 105
   end
 
-  @tag :p2
   test "Year 2016, Day 7, Part 2" do
     assert Solution.run_2() == 258
   end
