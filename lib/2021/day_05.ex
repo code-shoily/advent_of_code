e0a8d5e52469539d30a6c913d931cc57c78d26cd
Mafinar Khan <mafinar@gmail.com>
2021-12-05 02:58:57 -0500

    Refactor, shed some LoCs

--- lib/2021/day_05.ex
+++ lib/2021/day_05.ex
@@ -5,20 +5,18 @@ defmodule AdventOfCode.Y2021.Day05 do
   """
   use AdventOfCode.Helpers.InputReader, year: 2021, day: 5
 
-  def run_1, do: input!() |> parse() |> overlaps()
+  def run_1, do: input!() |> parse() |> overlaps(false)
   def run_2, do: input!() |> parse() |> overlaps(true)
-  def parse(data), do: Enum.map(String.split(data, "\n"), fn line -> ranges(line) end)
+  def parse(data), do: Enum.map(String.split(data, "\n"), &ranges/1)
 
-  defp ranges(line) do
-    line
-    |> String.split(" -> ")
-    |> then(fn [from, to] ->
-      {Enum.map(String.split(from, ","), &String.to_integer/1),
-       Enum.map(String.split(to, ","), &String.to_integer/1)}
-    end)
+  def ranges(line) do
+    ~r/(\d+),(\d+) -> (\d+),(\d+)/
+    |> Regex.run(line, capture: :all_but_first)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.split(2)
   end
 
-  defp overlaps(ranges, diagonal? \\ false) do
+  defp overlaps(ranges, diagonal?) do
     ranges
     |> Enum.flat_map(&points_between(&1, diagonal?))
     |> Enum.frequencies()
@@ -26,18 +24,14 @@ defmodule AdventOfCode.Y2021.Day05 do
   end
 
   defp points_between({from, to}, diagonal?) do
-    case {diagonal?, {from, to}} do
-      {_, {[a, b], [a, c]}} ->
-        Enum.map(b..c, &{a, &1})
-
-      {_, {[a, b], [c, b]}} ->
-        Enum.map(a..c, &{&1, b})
-
-      {true, {[a, b], [c, d]}} ->
-        Enum.map(0..abs(c - a), &{(a > c && a - &1) || a + &1, (b > d && b - &1) || b + &1})
-
-      _ ->
-        []
+    case {{from, to}, diagonal?} do
+      {{[same, a], [same, b]}, _} -> Enum.map(a..b, &{same, &1})
+      {{[a, same], [b, same]}, _} -> Enum.map(a..b, &{&1, same})
+      {range, true} -> diagonals(range)
+      _ -> []
     end
   end
+
+  defp diagonals({[a, b], [c, d]}),
+    do: Enum.map(0..abs(c - a), &{(a > c && a - &1) || a + &1, (b > d && b - &1) || b + &1})
 end
