012331977e62bf426ebef3f999f304783aa3f373
Mafinar Khan <mafinar@gmail.com>
2021-12-06 01:43:51 -0500

    Solve year 2021, day 6

--- lib/2021/day_06.ex
+++ lib/2021/day_06.ex
@@ -5,27 +5,23 @@ defmodule AdventOfCode.Y2021.Day06 do
   """
   use AdventOfCode.Helpers.InputReader, year: 2021, day: 6
 
-  def run_1, do: input!() |> parse() |> multiply(80) |> Map.values() |> Enum.sum()
-  def run_2, do: input!() |> parse() |> multiply(256) |> Map.values() |> Enum.sum()
+  def run_1, do: input!() |> parse() |> multiply(80) |> Enum.sum()
+  def run_2, do: input!() |> parse() |> multiply(256) |> Enum.sum()
 
   def parse(data),
     do: data |> String.split(",") |> Enum.map(&String.to_integer/1) |> Enum.frequencies()
 
-  def multiply(fishes, 0), do: fishes
-
   def multiply(fishes, day) do
-    fishes
-    |> Map.pop(0)
-    |> then(fn {reset, fishes} ->
+    (day == 0 && Map.values(fishes)) ||
       fishes
-      |> Enum.map(fn {k, v} -> {k - 1, v} end)
-      |> Enum.into(%{})
-      |> Map.merge(%{6 => reset, 8 => reset}, fn
-        _, nil, v -> v
-        _, v, nil -> v
-        _, v1, v2 -> v1 + v2
+      |> Map.pop(0)
+      |> then(fn {z, fishes} ->
+        Map.merge(
+          for({k, v} <- fishes, into: %{}, do: {k - 1, v}),
+          %{6 => z || 0, 8 => z || 0},
+          fn _, a, b -> a + b end
+        )
       end)
       |> multiply(day - 1)
-    end)
   end
 end
