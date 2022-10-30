14a0e26d97e1d8985035d1b7d9d60e9371e934bf
Byte Chakra <byte.chakra@gmail.com>
2022-10-30 00:11:58 -0400

    Mix format retcon

--- test/2016/day_09_test.exs
+++ test/2016/day_09_test.exs
@@ -33,7 +33,7 @@ defmodule AdventOfCode.Y2016.Day09Test do
       {"(3x3)XYZ", 9},
       {"X(8x2)(3x3)ABCY", 20},
       {"(27x12)(20x12)(13x14)(7x10)(1x12)A", 241_920},
-      {"(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445},
+      {"(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445}
     ]
 
     for {input, output} <- params do
