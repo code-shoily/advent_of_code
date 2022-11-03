779655e22267792118d53e43dfd44294b9176395
Byte Chakra <byte.chakra@gmail.com>
2022-11-02 23:26:16 -0400

    Add some input transformers

--- lib/helpers/transformers.ex
+++ lib/helpers/transformers.ex
@@ -3,6 +3,118 @@ defmodule AdventOfCode.Helpers.Transformers do
   Module to help transform data structures
   """
 
+  @doc ~S"""
+  Extracts lines from a string.
+
+  ## Example
+
+    iex> Transformers.lines("")
+    []
+
+    iex> Transformers.lines("Hello")
+    ["Hello"]
+
+    iex> Transformers.lines("Line 1\nLine 2")
+    ["Line 1", "Line 2"]
+
+    iex> Transformers.lines("Line 1\nLine 2\n ")
+    ["Line 1", "Line 2"]
+
+  """
+  def lines(raw) do
+    raw
+    |> String.split("\n", trim: true)
+    |> Enum.reject(fn str ->
+      String.trim(str) == ""
+    end)
+  end
+
+  @doc ~S"""
+  Extracts lines as ints from a string.
+
+  ## Example
+
+    iex> Transformers.int_lines("")
+    []
+
+    iex> Transformers.int_lines("1")
+    [1]
+
+    iex> Transformers.int_lines("1\n2")
+    [1, 2]
+
+    iex> Transformers.int_lines("1\n2\n ")
+    [1, 2]
+
+  """
+  def int_lines(raw) do
+    raw
+    |> lines()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Decomposes a string or number into digits.
+
+  ## Example
+
+    iex> Transformers.digits(123)
+    [1, 2, 3]
+
+    iex> Transformers.digits("123")
+    [1, 2, 3]
+
+    iex> Transformers.digits("")
+    []
+
+    iex> Transformers.digits(0)
+    [0]
+
+  """
+  def digits(raw) when is_integer(raw), do: raw |> to_string() |> digits()
+
+  def digits(raw) do
+    raw
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Separates a string as words, based on separator. Defaults to space.
+
+  ## Example
+
+    iex> Transformers.words("a, b, c", ",")
+    ["a", "b", "c"]
+
+    iex> Transformers.words("a b c")
+    ["a", "b", "c"]
+
+  """
+  def words(sentence, sep \\ " ") do
+    sentence
+    |> String.split(sep, trim: true)
+    |> Enum.map(&String.trim/1)
+  end
+
+  @doc """
+  Separates a string as words, based on separator. Defaults to space.
+
+  ## Example
+
+    iex> Transformers.int_words("1, 2, 3", ",")
+    [1, 2, 3]
+
+    iex> Transformers.int_words("1 2 3")
+    [1, 2, 3]
+
+  """
+  def int_words(sentence, sep \\ " ") do
+    sentence
+    |> words(sep)
+    |> Enum.map(&String.to_integer/1)
+  end
+
   @doc """
   Converts a list of list into a map of map.
 
