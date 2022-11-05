5c8b09b86bbcdb52e03428343ca361a9e26bf043
code-shoily <code.shoily@gmail.com>
2022-11-04 23:25:40 -0400

    Solve 2015/19-2 grrr

--- lib/2015/day_19.ex
+++ lib/2015/day_19.ex
@@ -2,6 +2,9 @@ defmodule AdventOfCode.Y2015.Day19 do
   @moduledoc """
   --- Day 19: Medicine for Rudolph ---
   Problem Link: https://adventofcode.com/2015/day/19
+
+  Helpful Tips for Part II: (,) analogy
+  https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju
   """
   use AdventOfCode.Helpers.InputReader, year: 2015, day: 19
 
@@ -10,18 +13,7 @@ defmodule AdventOfCode.Y2015.Day19 do
   def run(input \\ input!()) do
     input = parse(input)
 
-    {run_1(input), run_2(input)}
-  end
-
-  defp run_1({molecule, replacements}) do
-    replacements
-    |> Enum.map(&replace_by_one(molecule, &1))
-    |> Enum.reduce(&MapSet.union/2)
-    |> Enum.count()
-  end
-
-  defp run_2(_input) do
-    {:todo, 2}
+    {distinct_molecule_count(input), least_fabrication_steps(input)}
   end
 
   def parse(data \\ input!()) do
@@ -31,34 +23,38 @@ defmodule AdventOfCode.Y2015.Day19 do
   end
 
   defp parse_replacements(replacements) do
-    replacements
-    |> Enum.map(fn replacement ->
-      List.to_tuple(String.split(replacement, " => "))
-    end)
+    Enum.map(replacements, &List.to_tuple(String.split(&1, " => ")))
   end
 
-  def replace_by_one(molecule, {find, replace}) when is_binary(molecule) do
-    case String.split(molecule, find) do
-      [_] -> MapSet.new()
+  defp distinct_molecule_count({molecule, replacements}) do
+    replacements
+    |> Enum.map(&replace_by_one(molecule, &1))
+    |> Enum.reduce(&MapSet.union/2)
+    |> Enum.count()
+  end
 
-      matched ->
-        idxs = Enum.into(Range.new(1, (length(matched) - 1) * 2, 2), [])
+  defp least_fabrication_steps({molecule, _}) do
+    molecule = List.flatten(Regex.scan(~r/[A-Z][a-z]?/, molecule))
+    len = Enum.count(molecule)
+    parens = Enum.count(molecule, &(&1 == "Rn" || &1 == "Ar"))
+    comma = Enum.count(molecule, &(&1 == "Y"))
 
-        replace_by_one(
-          Enum.intersperse(matched, find),
-          idxs,
-          replace
-        )
+    len - parens - 2 * comma - 1
+  end
+
+  defp replace_by_one(molecule, {find, replace}) do
+    (Enum.count(match = String.split(molecule, find)) > 1 &&
+       replace_by_one(Enum.intersperse(match, find), indices(match), replace)) ||
+      MapSet.new()
+  end
+
+  defp replace_by_one(matches, indices, replacement) do
+    for index <- indices, into: %MapSet{} do
+      Enum.join(List.replace_at(matches, index, replacement), "")
     end
   end
 
-  def replace_by_one(matches, indices, replacement) do
-    indices
-    |> Enum.map(fn index ->
-      matches
-      |> List.replace_at(index, replacement)
-      |> Enum.join("")
-    end)
-    |> Enum.into(%MapSet{})
+  defp indices(matches) do
+    for i <- Range.new(1, (length(matches) - 1) * 2, 2), do: i
   end
 end
--- test/2015/day_19_test.exs
+++ test/2015/day_19_test.exs
@@ -7,6 +7,6 @@ defmodule AdventOfCode.Y2015.Day19Test do
   alias AdventOfCode.Y2015.Day19, as: Solution
 
   test "Year 2015, Day 19 run/1" do
-    assert Solution.run() == {nil, nil}
+    assert Solution.run() == {535, 212}
   end
 end
