9c97057fc50397cd1de2fe7afb8149d72db49e2a
Code Shoily <5702155+code-shoily@users.noreply.github.com>
2022-11-05 19:40:32 -0400

    Solve 2015/25 (#35)
    Co-authored-by: code-shoily <code.shoily@gmail.com>

---
+++ lib/2015/day_25.ex
@@ -0,0 +1,44 @@
+defmodule AdventOfCode.Y2015.Day25 do
+  @moduledoc """
+  --- Day 25: Let It Snow ---
+  Problem Link: https://adventofcode.com/2015/day/25
+  """
+  use AdventOfCode.Helpers.InputReader, year: 2015, day: 25
+
+  def run(input \\ input!()) do
+    input
+    |> parse()
+    |> index_of()
+    |> nth_code()
+  end
+
+  def index_of({row, 1}) do
+    1..row
+    |> Enum.reduce(%{0 => 1}, fn row, codes ->
+      Map.merge(codes, %{row => Map.get(codes, row - 1) + (row - 1)})
+    end)
+    |> Map.get(row)
+  end
+
+  def index_of({row, col}) do
+    2..col
+    |> Enum.reduce(%{1 => index_of({row, 1})}, fn col, codes ->
+      Map.merge(codes, %{col => Map.get(codes, col - 1) + row + (col - 1)})
+    end)
+    |> Map.get(col)
+  end
+
+  def nth_code(1), do: 20_151_125
+
+  def nth_code(n) do
+    Enum.reduce(2..n, nth_code(1), fn _, code ->
+      rem(code * 252_533, 33_554_393)
+    end)
+  end
+
+  def parse(data \\ input!()) do
+    Regex.run(~r/\D+(\d+)\D+(\d+)/, data, capture: :all_but_first)
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+end
---
+++ test/2015/day_25_test.exs
@@ -0,0 +1,12 @@
+defmodule AdventOfCode.Y2015.Day25Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y1525
+
+  alias AdventOfCode.Y2015.Day25, as: Solution
+
+  test "Year 2015, Day 25 run/1" do
+    assert Solution.run() == 19_980_801
+  end
+end
