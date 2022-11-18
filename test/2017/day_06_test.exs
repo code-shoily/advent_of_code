632065d16720be99a01f1a8621a074fa2e5c1d85
code-shoily <code.shoily@gmail.com>
2022-11-18 02:53:36 -0500

    Add _ in larger number

--- test/2017/day_06_test.exs
+++ test/2017/day_06_test.exs
@@ -7,6 +7,6 @@ defmodule AdventOfCode.Y2017.Day06Test do
   alias AdventOfCode.Y2017.Day06, as: Solution
 
   test "Year 2017, Day 6 run/1" do
-    assert Solution.run() == {11137, 1037}
+    assert Solution.run() == {11_137, 1037}
   end
 end
