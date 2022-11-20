9b39260012a86506475a3b9d18e772bed618798b
code-shoily <code.shoily@gmail.com>
2022-11-20 00:03:12 -0500

    Refactor a little

--- lib/2017/day_16.ex
+++ lib/2017/day_16.ex
@@ -7,62 +7,44 @@ defmodule AdventOfCode.Y2017.Day16 do
 
   alias AdventOfCode.Helpers.Transformers
 
-  @dancer_count 16
+  @dancers to_string(Enum.take(?a..?z, 16))
 
   def run(input \\ input!()) do
-    dancers = ?a..?z |> Enum.take(@dancer_count) |> to_string()
     moves = parse(input)
 
-    {dance(dancers, moves), perform_billionth(dancers, moves)}
+    {dance(@dancers, moves), perform_billionth(@dancers, moves)}
   end
 
   defp perform_billionth(dancers, moves) do
     count = rem(1_000_000_000, repeats_after(dancers, moves))
-
-    Enum.reduce(1..count, dancers, fn _, dancers ->
-      dance(dancers, moves)
-    end)
-  end
-
-  def repeats_after(dancers, moves), do: repeats_after(dancers, moves, 0, MapSet.new())
-
-  def repeats_after(dancers, moves, count, done) do
-    dancers = dance(dancers, moves)
-
-    (MapSet.member?(done, dancers) && count) ||
-      repeats_after(dancers, moves, count + 1, MapSet.put(done, dancers))
+    Enum.reduce(1..count, dancers, fn _, dancers -> dance(dancers, moves) end)
   end
 
   def parse(data \\ input!()) do
-    data
-    |> Transformers.words(",")
-    |> Enum.map(&parse_move/1)
+    for m <- Transformers.words(data, ","), do: parse_move(m)
   end
 
-  defp parse_move("s" <> size) do
-    {:spin, String.to_integer(size)}
-  end
+  @re ~r"^(.+)\/(.+)$"
+  defp parse_move("s" <> size), do: {:spin, String.to_integer(size)}
 
-  @regex ~r"^(.+)\/(.+)$"
+  defp parse_move("p" <> rest),
+    do: {:partner, Enum.sort(Regex.run(@re, rest, capture: :all_but_first))}
+
   defp parse_move("x" <> rest) do
     {:exchange,
-     @regex
+     @re
      |> Regex.run(rest, capture: :all_but_first)
-     |> then(&(&1 |> Enum.map(fn i -> String.to_integer(i) end) |> Enum.sort()))}
+     |> then(&Enum.sort(Enum.map(&1, fn i -> String.to_integer(i) end)))}
   end
 
-  defp parse_move("p" <> rest) do
-    {:partner, Enum.sort(Regex.run(@regex, rest, capture: :all_but_first))}
-  end
+  defp dance(dancers, []), do: dancers
 
-  def dance(dancers, []), do: dancers
-
-  def dance(dancers, [move | moves]) do
+  defp dance(dancers, [move | moves]) do
     # So many repeating words :D
     dance(dance(dancers, move), moves)
   end
 
-  def dance(dancers, {:exchange, [a, b]}) do
+  defp dance(dancers, {:exchange, [a, b]}) do
     Enum.join([
       String.slice(dancers, 0, a),
       String.at(dancers, b),
@@ -72,14 +54,23 @@ defmodule AdventOfCode.Y2017.Day16 do
     ])
   end
 
-  def dance(dancers, {:spin, a}) do
+  defp dance(dancers, {:spin, a}) do
     String.slice(dancers, -a..-1) <> String.slice(dancers, 0..(-a - 1))
   end
 
-  def dance(dancers, {:partner, [a, b]}) do
+  defp dance(dancers, {:partner, [a, b]}) do
     dancers
     |> String.replace(a, "_")
     |> String.replace(b, a)
     |> String.replace("_", b)
+  end
+
+  defp repeats_after(dancers, moves), do: repeats_after(dancers, moves, 0, MapSet.new())
+
+  defp repeats_after(dancers, moves, count, done) do
+    dancers = dance(dancers, moves)
+
+    (MapSet.member?(done, dancers) && count) ||
+      repeats_after(dancers, moves, count + 1, MapSet.put(done, dancers))
   end
 end
