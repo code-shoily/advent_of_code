e0597c0cce1257e1957ffc0b3bb4f52d27686e07
code-shoily <code.shoily@gmail.com>
2022-11-27 17:17:42 -0500

    Formatter did it

--- lib/helpers/input_reader.ex
+++ lib/helpers/input_reader.ex
@@ -4,7 +4,8 @@ defmodule AdventOfCode.Helpers.InputReader do
   """
   @input_dir Path.join(:code.priv_dir(:advent_of_code), "input_files")
 
-  @doc deprecated: "Use `read_from_file` instead. This was created long time ago with different goals in mind."
+  @doc deprecated:
+         "Use `read_from_file` instead. This was created long time ago with different goals in mind."
   defmacro __using__(year: year, day: day) do
     quote do
       defp build_path(year, day), do: "#{unquote(@input_dir)}/#{year}_#{day}.txt"
