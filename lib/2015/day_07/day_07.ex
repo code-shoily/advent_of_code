e023a01e1f724744920dd3885810eb4fdad5239c
code-shoily <code.shoily@gmail.com>
2022-11-30 22:31:05 -0500

    Demacro 2015/7

--- lib/2015/day_07/day_07.ex
+++ lib/2015/day_07/day_07.ex
@@ -3,13 +3,14 @@ defmodule AdventOfCode.Y2015.Day07 do
   --- Day 7: Some Assembly Required ---
   Problem Link: https://adventofcode.com/2015/day/7
   """
-  use AdventOfCode.Helpers.InputReader, year: 2015, day: 7
-
+  alias AdventOfCode.Helpers.InputReader
   alias AdventOfCode.Y2015.Day07.ControlPanel
+
+  @input InputReader.read_from_file(2015, 7)
 
   @bin_ops ~w/RSHIFT LSHIFT AND OR/
 
-  def run(input \\ input!()) do
+  def run(input \\ @input) do
     parsed_input = parse(input)
     solution_1 = run_1(parsed_input)
     solution_2 = run_2(parsed_input, solution_1)
@@ -31,7 +32,7 @@ defmodule AdventOfCode.Y2015.Day07 do
     |> process_and_get_signal("a")
   end
 
-  def parse(data \\ input!()) do
+  def parse(data \\ @input) do
     data
     |> String.split("\n", trim: true)
     |> Enum.map(&tokenize/1)
