a973d80e1e4dd8644106018c4c9953ccdb871d43
code-shoily <code.shoily@gmail.com>
2022-12-03 00:55:31 -0500

    Solve 2022/3

---
+++ lib/2022/day_03.ex
@@ -0,0 +1,52 @@
+defmodule AdventOfCode.Y2022.Day03 do
+  @moduledoc """
+  --- Day 3: Rucksack Reorganization ---
+  Problem Link: https://adventofcode.com/2022/day/3
+  """
+  alias AdventOfCode.Helpers.{InputReader, Transformers}
+
+  def input, do: InputReader.read_from_file(2022, 3)
+
+  def run(input \\ input()) do
+    rucksacks = Transformers.lines(input)
+
+    {total_priority(compartment_priorities(rucksacks)),
+     total_priority(group_priorities(rucksacks))}
+  end
+
+  defp compartment_priorities(rucksacks), do: Enum.map(rucksacks, &in_both_compartments/1)
+
+  defp group_priorities(rucksacks) do
+    rucksacks
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn [x | xs] ->
+      Enum.reduce(xs, as_set(x), fn rucksack, rucksacks ->
+        rucksack |> as_set() |> MapSet.intersection(rucksacks)
+      end)
+    end)
+  end
+
+  defp in_both_compartments(string) do
+    mid = Integer.floor_div(String.length(string), 2)
+
+    [String.slice(string, 0, mid), String.slice(string, mid, mid)]
+    |> Enum.map(&MapSet.new(String.codepoints(&1)))
+    |> then(fn [a, b] -> MapSet.intersection(a, b) end)
+  end
+
+  defp as_set(rucksack), do: MapSet.new(String.codepoints(rucksack))
+
+  defp priority(char) do
+    case :binary.first(char) do
+      lower when lower >= ?a and lower <= ?z -> lower - ?a + 1
+      upper when upper >= ?A and upper <= ?Z -> upper - ?A + 27
+    end
+  end
+
+  defp total_priority(common_items) do
+    Enum.reduce(common_items, 0, fn x, acc ->
+      [common_item] = MapSet.to_list(x)
+      acc + priority(common_item)
+    end)
+  end
+end
---
+++ test/2022/day_03_test.exs
@@ -0,0 +1,12 @@
+defmodule AdventOfCode.Y2022.Day03Test do
+  @moduledoc false
+
+  use ExUnit.Case
+  @moduletag :y2203
+
+  alias AdventOfCode.Y2022.Day03, as: Solution
+
+  test "Year 2022, Day 3 run/1" do
+    assert Solution.run() == {8233, 2821}
+  end
+end
