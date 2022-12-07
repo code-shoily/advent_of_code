bde867790cff8aa564a588bc136b6ee7e5e48f47
Code Shoily <5702155+code-shoily@users.noreply.github.com>
2022-12-06 22:22:01 -0500

    2015 6 (#40)
    * Solve 2015/6
    
    * Solve 2015/6
    
    * Update 2015/6 with Aja
    
    Co-authored-by: code-shoily <code.shoily@gmail.com>

--- lib/2015/day_06.ex
+++ lib/2015/day_06.ex
@@ -4,99 +4,48 @@ defmodule AdventOfCode.Y2015.Day06 do
   Problem Link: https://adventofcode.com/2015/day/6
   NOTE: This is slow!
   """
+  alias Aja.Vector
   alias AdventOfCode.Helpers.{InputReader, Transformers}
 
   def input, do: InputReader.read_from_file(2015, 6)
 
   def run(input \\ input()) do
-    input = parse(input)
+    input = Enum.map(Transformers.lines(input), &parse_input/1)
     grid = make_grid(1000)
 
-    task_1 = Task.async(fn -> run_1(input, grid) end)
-    task_2 = Task.async(fn -> run_2(input, grid) end)
+    task_1 = Task.async(fn -> brightness(Enum.reduce(input, grid, &apply_1/2)) end)
+    task_2 = Task.async(fn -> brightness(Enum.reduce(input, grid, &apply_2/2)) end)
 
     {Task.await(task_1, :infinity), Task.await(task_2, :infinity)}
   end
 
-  def parse(input) do
-    input
-    |> Transformers.lines()
-    |> Enum.map(&parse_input/1)
-  end
+  def apply_1(line, grid),
+    do: apply(line, grid, fn _ -> 1 end, fn _ -> 0 end, fn v -> (v == 0 && 1) || 0 end)
 
-  defp run_1(input, grid) do
-    input
-    |> Enum.reduce(grid, &apply_1(&1, &2))
-    |> total_brightness()
-  end
+  def apply_2(line, grid), do: apply(line, grid, &(&1 + 1), &max(0, &1 - 1), &(&1 + 2))
 
-  defp run_2(input, grid) do
-    input
-    |> Enum.reduce(grid, &apply_2(&1, &2))
-    |> total_brightness()
-  end
-
-  def make_grid(dim) do
-    0..(dim - 1)
-    |> Stream.map(fn x ->
-      {x, 0..(dim - 1) |> Stream.map(&{&1, 0}) |> Enum.into(%{})}
+  defp apply({cmd, xs, ys}, grid, on, off, toggle) do
+    Vector.foldl(coords(xs, ys), grid, fn [x, y], acc ->
+      case cmd do
+        :turn_on -> Vector.update_at(acc, x, &Vector.update_at(&1, y, on))
+        :turn_off -> Vector.update_at(acc, x, &Vector.update_at(&1, y, off))
+        :toggle -> Vector.update_at(acc, x, &Vector.update_at(&1, y, toggle))
+      end
     end)
-    |> Enum.into(%{})
   end
 
-  @regex ~r"""
-  (?<cmd>toggle|turn\son|turn\soff)\s
-  (?<x1>\d+),(?<y1>\d+)
-  \s\S+\s
-  (?<x2>\d+),(?<y2>\d+)
-  """x
-  defp parse_input(line) do
-    @regex
-    |> Regex.named_captures(line)
-    |> format()
-  end
+  defp make_grid(dim), do: Vector.duplicate(Vector.duplicate(0, dim), dim)
+  defp as_atom("turn on"), do: :turn_on
+  defp as_atom("turn off"), do: :turn_off
+  defp as_atom("toggle"), do: :toggle
+  defp brightness(grid), do: Aja.Enum.reduce(grid, 0, &(&2 + Aja.Enum.sum(&1)))
+  defp coords(xs, ys), do: Vector.new(for x <- xs, y <- ys, do: [x, y])
+
+  @regex ~r"(?<cmd>toggle|turn\son|turn\soff)\s(?<x1>\d+),(?<y1>\d+)\s\S+\s(?<x2>\d+),(?<y2>\d+)"
+  defp parse_input(line), do: format(Regex.named_captures(@regex, line))
 
   defp format(%{"cmd" => cmd, "x1" => x1, "x2" => x2, "y1" => y1, "y2" => y2}) do
-    {
-      cmd |> String.replace(" ", "_") |> String.to_atom(),
-      {String.to_integer(x1), String.to_integer(y1)},
-      {String.to_integer(x2), String.to_integer(y2)}
-    }
-  end
-
-  defp apply_1(:turn_on, coord, src), do: update_in(src, coord, fn _ -> 1 end)
-  defp apply_1(:turn_off, coord, src), do: update_in(src, coord, fn _ -> 0 end)
-  defp apply_1(:toggle, coord, src), do: update_in(src, coord, fn v -> (v == 0 && 1) || 0 end)
-
-  defp apply_1({cmd, {x1, y1}, {x2, y2}}, src) do
-    x1..x2
-    |> Stream.flat_map(fn x ->
-      y1..y2
-      |> Stream.map(fn y ->
-        [x, y]
-      end)
-    end)
-    |> Enum.reduce(src, fn x, acc -> apply_1(cmd, x, acc) end)
-  end
-
-  defp total_brightness(grid) do
-    grid
-    |> Stream.flat_map(fn {_, line} ->
-      Stream.map(line, fn {_, value} -> value end)
-    end)
-    |> Enum.sum()
-  end
-
-  defp apply_2(:turn_on, coord, src), do: update_in(src, coord, fn v -> v + 1 end)
-
-  defp apply_2(:turn_off, coord, src),
-    do: update_in(src, coord, fn v -> (v - 1 < 0 && 0) || v - 1 end)
-
-  defp apply_2(:toggle, coord, src), do: update_in(src, coord, fn v -> v + 2 end)
-
-  defp apply_2({cmd, {x1, y1}, {x2, y2}}, src) do
-    x1..x2
-    |> Stream.flat_map(&Stream.map(y1..y2, fn y -> [&1, y] end))
-    |> Enum.reduce(src, fn x, acc -> apply_2(cmd, x, acc) end)
+    {as_atom(cmd), String.to_integer(x1)..String.to_integer(x2),
+     String.to_integer(y1)..String.to_integer(y2)}
   end
 end
--- test/2015/day_06_test.exs
+++ test/2015/day_06_test.exs
@@ -1,11 +1,10 @@
 defmodule AdventOfCode.Y2015.Day06Test do
   @moduledoc false
   use ExUnit.Case
-  @moduletag :y1506_slow
+  @moduletag :y1506
 
   alias AdventOfCode.Y2015.Day06, as: Solution
 
-  @tag :skip_slow
   test "Year 2015, Day 6" do
     assert Solution.run() == {377_891, 14_110_788}
   end
