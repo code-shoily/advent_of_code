f0878b43a15e0155debc404c186322ce30f3c6e3
Mafinar K <mafinar@gmail.com>
2023-04-30 09:31:45 -0400

    Remove input macro. Fixes #39

--- lib/helpers/input_reader.ex
+++ lib/helpers/input_reader.ex
@@ -4,21 +4,6 @@ defmodule AdventOfCode.Helpers.InputReader do
   """
   @input_dir Path.join(:code.priv_dir(:advent_of_code), "input_files")
 
-  @doc deprecated:
-         "Use `read_from_file` instead. This was created long time ago with different goals in mind."
-  defmacro __using__(year: year, day: day) do
-    quote do
-      defp build_path(year, day), do: "#{unquote(@input_dir)}/#{year}_#{day}.txt"
-
-      def read!(year, day) do
-        year |> build_path(day) |> File.read!()
-      end
-
-      def input!, do: read!(unquote(year), unquote(day)) |> String.trim()
-      def input_notrim!, do: read!(unquote(year), unquote(day))
-    end
-  end
-
   @doc """
   Reads input from file. Reads from the input directory `priv/input_files` and sends
   a trimmed string if `trim` is true otherwise returns the data as raw. The reason `trim`
