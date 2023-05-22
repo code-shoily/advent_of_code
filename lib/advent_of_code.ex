969d93f7f35a63d1ddb68c6a76f6779ecc4bd9a8
Mafinar K <mafinar@gmail.com>
2023-05-21 23:49:41 -0400

    Add solution function

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,3 +1,29 @@
 defmodule AdventOfCode do
-  @moduledoc false
+  @moduledoc """
+  Module that solves problem given year and day.
+  """
+
+  @latest_year 2022
+
+  @type year() :: 2015 | 2016 | 2017 | 2018 | 2019 | 2020 | 2021 | 2022
+  @type day() :: non_neg_integer()
+
+  @doc """
+  Solves for `year` and `day`.
+  """
+  @spec solve(year(), day()) :: {any(), any()}
+  def solve(year, day) do
+    Module.concat([AdventOfCode, get_year_module(year), get_day_module(day)]).run()
+  end
+
+  defp get_year_module(year) when year >= 2015 and year <= @latest_year do
+    "Y" <> Integer.to_string(year)
+  end
+
+  defp get_day_module(day) when day >= 1 and day <= 25 do
+    day
+    |> Integer.to_string()
+    |> String.pad_leading(2, "0")
+    |> then(fn day -> "Day" <> day end)
+  end
 end
