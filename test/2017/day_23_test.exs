226a7ea852e51b068b6a36112fbda3e2d1bfd692
Mafinar K <mafinar@gmail.com>
2023-06-20 07:40:06 -0400

    Fix test placeholder

--- test/2017/day_23_test.exs
+++ test/2017/day_23_test.exs
@@ -7,6 +7,6 @@ defmodule AdventOfCode.Y2017.Day23Test do
   alias AdventOfCode.Y2017.Day23, as: Solution
 
   test "Year 2017, Day 23 run/1" do
-    assert Solution.run() == {6241, nil}
+    assert Solution.run() == {6241, {:todo, 2}}
   end
 end
