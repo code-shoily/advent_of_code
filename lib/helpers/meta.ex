218af28eb50af75c9f9eab4a5f31b9be3a5727fd
Mafinar K <mafinar@gmail.com>
2023-10-09 19:50:23 -0400

    Add summarizer

--- lib/helpers/meta.ex
+++ lib/helpers/meta.ex
@@ -1,13 +1,76 @@
 defmodule AdventOfCode.Helpers.Meta do
-  def get_solution_count(year, day) do
+  @doc """
+  Returns the info of the year. This returns the important links and info
+  of all the days completed.
+  """
+  def get_info(year) do
+    daywise_summary =
+      for day <- 1..25 do
+        get_info_for_day(year, day)
+      end
+      |> Enum.reject(&is_nil/1)
+
+    stars_completed =
+      daywise_summary
+      |> Enum.map(fn {_, %{count: count}} -> count end)
+      |> Enum.sum()
+
+    %{
+      completed: stars_completed,
+      link: "http://adventofcode/#{year}",
+      test: "/test/#{year}/",
+      summary: daywise_summary
+    }
+  end
+
+  @doc """
+  Returns the information for problem year/day. If this was not attempted then
+  `nil` is returned. Otherwise a summary of links, solution status, title etc are
+  returned.
+  """
+  def get_info_for_day(year, day) do
+    case get_solution_count(year, day) do
+      0 ->
+        nil
+
+      solution_count ->
+        {day,
+         %{
+           year: year,
+           day: day,
+           link: "https://adventofcode.com/#{year}/day/#{day}",
+           solution: "/lib/#{year}/day_#{padded(day)}.ex",
+           test: "/test/#{year}/day_#{padded(day)}_test.exs",
+           title: title(year, day),
+           count: solution_count
+         }}
+    end
+  end
+
+  defp title(year, day) do
+    "Elixir.AdventOfCode.Y#{year}.Day#{padded(day)}"
+    |> String.to_existing_atom()
+    |> Code.fetch_docs()
+    |> elem(4)
+    |> Map.fetch!("en")
+    |> String.split("\n")
+    |> hd()
+    |> then(fn padded_title ->
+      ~r/--- Day (\d+): (?<name>.+) ---/
+      |> Regex.named_captures(padded_title)
+      |> Map.fetch!("name")
+    end)
+  end
+
+  defp get_solution_count(year, day) do
     year
-    |> ast_at_path(day)
+    |> ast_at_test_path(day)
     |> get_solution_count(year, day)
   end
 
-  def get_solution_count(nil, _, _), do: 0
+  defp get_solution_count(nil, _, _), do: 0
 
-  def get_solution_count({:ok, {:defmodule, _, module_content}}, year, day) do
+  defp get_solution_count({:ok, {:defmodule, _, module_content}}, year, day) do
     with [_, [{:do, do_block}]] <- module_content,
          {:__block__, _, cases} = do_block do
       cases
@@ -35,7 +98,7 @@ defmodule AdventOfCode.Helpers.Meta do
     end
   end
 
-  defp ast_at_path(year, day) do
+  defp ast_at_test_path(year, day) do
     case File.read("test/#{year}/day_#{padded(day)}_test.exs") do
       {:ok, content} -> content |> Code.string_to_quoted()
       _ -> nil
