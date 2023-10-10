bd746c226a3237d7259ec3ab10c832b0737701e0
Mafinar K <mafinar@gmail.com>
2023-10-09 20:42:04 -0400

    Rename stat generator task

---
+++ lib/mix/tasks/gen_stats.ex
@@ -0,0 +1,33 @@
+defmodule Mix.Tasks.GenStats do
+  @moduledoc """
+  Updates the README.md for all years.
+
+  Type `mix gen_stats` to update all `year/README.md`.
+
+  For example, `mix gen_stats` will generate `lib/<year>/README.md` of all years.
+  """
+
+  alias AdventOfCode.Helpers.Summarizer
+
+  use Mix.Task
+
+  @year_range 2015..2022
+
+  @usage "mix gen_stats"
+
+  @shortdoc """
+  Creates the per year README.md with updated solution status.
+  """
+  @impl Mix.Task
+  def run(_) do
+    for year <- @year_range do
+      readme_content = Summarizer.yearwise_readme(year)
+
+      File.write!("lib/#{year}/README.md", readme_content)
+
+      IO.puts("#{year}/README.md successfully updated!")
+    end
+  rescue
+    _ -> IO.puts(@usage)
+  end
+end
