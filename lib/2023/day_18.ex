936482d893d5d6826079cf6a398696a4890362af
Mafinar K <mafinar@gmail.com>
2023-12-22 10:28:19 -0500

    Refactor 23/18

--- lib/2023/day_18.ex
+++ lib/2023/day_18.ex
@@ -9,23 +9,21 @@ defmodule AdventOfCode.Y2023.Day18 do
 
   import AdventOfCode.Algorithms.Geometry, only: [polygon_area: 1]
 
-  @dir_mapper %{
-    "R" => :right,
-    "D" => :down,
-    "L" => :left,
-    "U" => :up,
-    "0" => :right,
-    "1" => :down,
-    "2" => :left,
-    "3" => :up
-  }
-  @origin {0, 0}
+  @dir_map_1 %{"R" => :right, "D" => :down, "L" => :left, "U" => :up}
+  @dir_map_2 %{"0" => :right, "1" => :down, "2" => :left, "3" => :up}
 
   def input, do: InputReader.read_from_file(2023, 18)
 
   def run(input \\ input()) do
     input = parse(input)
-    {lava_capacity(input), lava_capacity(correct(input))}
+    {input |> skip_colour() |> lava_capacity(), input |> rectify() |> lava_capacity()}
+  end
+
+  def parse(data \\ input()) do
+    for line <- Transformers.lines(data) do
+      [_, dir, steps, colours] = Regex.run(~r{(.) (\d+) \((.+)\)}, line)
+      {@dir_map_1[dir], String.to_integer(steps), colours}
+    end
   end
 
   defp lava_capacity(input) do
@@ -33,36 +31,30 @@ defmodule AdventOfCode.Y2023.Day18 do
     trunc(polygon_area(polygon) + div(perimeter, 2) + 1)
   end
 
-  def move(rules), do: move(rules, @origin, [@origin])
-  def move([], _, points), do: Enum.reverse(points)
+  defp move(rules), do: move(rules, {0, 0}, [{0, 0}])
+  defp move([], _, points), do: Enum.reverse(points)
 
-  def move([{:up, steps, _} | rules], {x, y}, points),
+  defp move([{:up, steps} | rules], {x, y}, points),
     do: move(rules, {x - steps, y}, [{x - steps, y} | points])
 
-  def move([{:down, steps, _} | rules], {x, y}, points),
+  defp move([{:down, steps} | rules], {x, y}, points),
     do: move(rules, {x + steps, y}, [{x + steps, y} | points])
 
-  def move([{:left, steps, _} | rules], {x, y}, points),
+  defp move([{:left, steps} | rules], {x, y}, points),
     do: move(rules, {x, y - steps}, [{x, y - steps} | points])
 
-  def move([{:right, steps, _} | rules], {x, y}, points),
+  defp move([{:right, steps} | rules], {x, y}, points),
     do: move(rules, {x, y + steps}, [{x, y + steps} | points])
 
-  def parse(data \\ input()) do
-    for line <- Transformers.lines(data) do
-      [_, dir, steps, colours] = Regex.run(~r{(.) (\d+) \((.+)\)}, line)
-      {@dir_mapper[dir], String.to_integer(steps), colours}
-    end
-  end
-
-  def get_geometry(instructions) do
+  defp get_geometry(instructions) do
     perimeter = Enum.reduce(instructions, 0, fn x, acc -> acc + elem(x, 1) end)
 
     {perimeter, move(instructions)}
   end
 
-  def process_colour(<<"#", steps::binary-size(5), dir::binary-1>>),
-    do: {@dir_mapper[dir], elem(Integer.parse(steps, 16), 0), :discard}
+  defp process_colour(<<"#", steps::binary-size(5), dir::binary-1>>),
+    do: {@dir_map_2[dir], elem(Integer.parse(steps, 16), 0)}
 
-  def correct(instructions), do: Enum.map(instructions, &process_colour(elem(&1, 2)))
+  defp skip_colour(instructions), do: Enum.map(instructions, fn {a, b, _} -> {a, b} end)
+  defp rectify(instructions), do: Enum.map(instructions, &process_colour(elem(&1, 2)))
 end
