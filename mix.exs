a5d29d7ff97cad064a138c82aef90f3435eaa2e9
Mafinar K <mafinar@gmail.com>
2023-12-22 18:44:45 -0500

    Upgrade elixir and erlang

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCode.MixProject do
     [
       app: :advent_of_code,
       version: "0.20.0",
-      elixir: "~> 1.15.0",
+      elixir: "~> 1.16.0",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -23,8 +23,8 @@ defmodule AdventOfCode.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:floki, "~> 0.34.0"},
-      {:httpoison, "~> 2.1.0"},
+      {:floki, "~> 0.35.0"},
+      {:httpoison, "~> 2.2.0"},
       {:jason, "~> 1.4"},
       {:libgraph, "~> 0.16"},
       {:aja, "~> 0.6.1"},
