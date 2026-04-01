6f02dff7ecf2a014adea17c6dddc2284df7d2132
Mafinar Khan <mafinar@gmail.com>
2026-04-01 19:33:00 -0400

    Refactor away from GenServer

--- lib/2015/day_07.ex
+++ lib/2015/day_07.ex
@@ -3,111 +3,84 @@ defmodule AdventOfCode.Y2015.Day07 do
   --- Day 7: Some Assembly Required ---
   Problem Link: https://adventofcode.com/2015/day/7
   Difficulty: m
-  Tags: genserver data-modelling op-code
+  Tags: graph dag logic-gates bitwise
   """
+  import Bitwise
   alias AdventOfCode.Helpers.{InputReader, Transformers}
-  alias AdventOfCode.Y2015.Day07.ControlPanel
 
   def input, do: InputReader.read_from_file(2015, 7)
 
-  @bin_ops ~w/RSHIFT LSHIFT AND OR/
-
   def run(input \\ input()) do
-    parsed_input = parse(input)
-    solution_1 = run_1(parsed_input)
-    solution_2 = run_2(parsed_input, solution_1)
+    wires = parse(input)
+
+    values_1 = solve_circuit(wires)
+    solution_1 = values_1["a"]
+
+    wires_2 = Map.put(wires, "b", {:assign, [solution_1]})
+    values_2 = solve_circuit(wires_2)
+    solution_2 = values_2["a"]
 
     {solution_1, solution_2}
   end
 
-  defp run_1(parsed_input) do
-    process_and_get_signal(parsed_input, "a")
+  defp solve_circuit(wires) do
+    edges =
+      for {target, {_op, args}} <- wires,
+          arg <- args,
+          is_binary(arg),
+          do: {arg, target, 1}
+
+    graph =
+      Map.keys(wires)
+      |> Enum.reduce(Yog.directed(), fn wire, g -> Yog.add_node(g, wire, nil) end)
+      |> Yog.add_edges!(edges)
+
+    {:ok, sorted} = Yog.Traversal.topological_sort(graph)
+
+    Enum.reduce(sorted, %{}, fn wire, values ->
+      {op, args} = wires[wire]
+
+      arg_vals =
+        Enum.map(args, fn
+          arg when is_integer(arg) -> arg
+          arg when is_binary(arg) -> Map.get(values, arg)
+        end)
+
+      Map.put(values, wire, compute(op, arg_vals))
+    end)
   end
 
-  defp run_2({assignments, relations}, override) do
-    assignments
-    |> Enum.map(fn
-      {"b", assignment} -> {"b", Map.merge(assignment, %{relation: {:assign, override}})}
-      assignment -> assignment
-    end)
-    |> then(fn assignments -> {assignments, relations} end)
-    |> process_and_get_signal("a")
-  end
+  defp compute(:assign, [x]), do: x
+  defp compute(:not, [x]), do: bnot(x) &&& 0xFFFF
+  defp compute(:and, [x, y]), do: x &&& y &&& 0xFFFF
+  defp compute(:or, [x, y]), do: (x ||| y) &&& 0xFFFF
+  defp compute(:lshift, [x, y]), do: x <<< y &&& 0xFFFF
+  defp compute(:rshift, [x, y]), do: x >>> y &&& 0xFFFF
 
   def parse(data \\ input()) do
     data
     |> Transformers.lines()
-    |> Enum.map(&tokenize/1)
-    |> Map.new(fn {instruction, wire} ->
-      {wire, %{relation: instruction, providers: get_providers(instruction)}}
-    end)
-    |> Enum.split_with(fn
-      {_, %{providers: []}} -> true
-      _ -> false
+    |> Map.new(fn line ->
+      [lhs, rhs] = String.split(line, " -> ")
+      {rhs, parse_op(lhs)}
     end)
   end
 
-  # --- <Solution Functions> ---
-  defp process_and_get_signal({assignments, relations} = instructions, wire) do
-    ControlPanel.start_servers()
-
-    ControlPanel.create_wires(instructions)
-    ControlPanel.build_dependencies(relations)
-    ControlPanel.provide_signals(assignments)
-
-    result = ControlPanel.get_signal(wire)
-
-    ControlPanel.stop_servers()
-
-    result
-  end
-
-  defp tokenize(line) do
-    line
-    |> String.split("->")
-    |> then(fn [lhs, rhs] ->
-      {parse_instruction(lhs), String.trim(rhs)}
-    end)
-  end
-
-  defp get_providers(relation) do
-    case relation do
-      {_, value} when is_binary(value) ->
-        [value]
-
-      {_, value_a, value_b} when is_binary(value_a) and is_binary(value_b) ->
-        [value_a, value_b]
-
-      {_, value, _} when is_binary(value) ->
-        [value]
-
-      {_, _, value} when is_binary(value) ->
-        [value]
-
-      _ ->
-        []
+  defp parse_op(s) do
+    case String.split(s) do
+      ["NOT", x] -> {:not, [val(x)]}
+      [x, "AND", y] -> {:and, [val(x), val(y)]}
+      [x, "OR", y] -> {:or, [val(x), val(y)]}
+      [x, "LSHIFT", y] -> {:lshift, [val(x), val(y)]}
+      [x, "RSHIFT", y] -> {:rshift, [val(x), val(y)]}
+      [x] -> {:assign, [val(x)]}
     end
   end
 
-  defp parse_instruction(s) do
-    case Regex.split(~r/\s+/, s, trim: true) do
-      ["NOT", value] ->
-        {:not, sanitize(value)}
-
-      [value_1, op, value_2] when op in @bin_ops ->
-        {String.to_atom(String.downcase(op)), sanitize(value_1), sanitize(value_2)}
-
-      [value] ->
-        {:assign, sanitize(value)}
+  defp val(s) do
+    case Integer.parse(s) do
+      {n, ""} -> n
+      _ -> s
     end
   end
-
-  defp sanitize(value) do
-    case Integer.parse(value) do
-      {value, ""} -> value
-      :error -> value
-    end
-  end
-
-  # --- </Solution Functions> ---
 end
--- lib/2016/day_10.ex
+++ lib/2016/day_10.ex
@@ -3,85 +3,88 @@ defmodule AdventOfCode.Y2016.Day10 do
   --- Day 10: Balance Bots ---
   Problem Link: https://adventofcode.com/2016/day/10
   Difficulty: m
-  Tags: data-modelling genserver
+  Tags: graph simulation dataflow
   """
   alias AdventOfCode.Helpers.{InputReader, Transformers}
-  alias __MODULE__.{Bot, BotSupervisor, ControlPanel, OutputBin}
 
   def input, do: InputReader.read_from_file(2016, 10)
 
-  @param [17, 61]
-  def run_1 do
-    BotSupervisor
-    |> Supervisor.which_children()
-    |> Enum.map(fn {_, pid, _, _} -> Bot.vals(pid) end)
-    |> Enum.find(fn
-      {_, @param} -> true
-      _ -> false
-    end)
-    |> elem(0)
-  end
-
-  @param ~w/0 1 2/
-  def run_2 do
-    OutputBin
-    |> :sys.get_state()
-    |> Map.take(@param)
-    |> Map.values()
-    |> Enum.product()
-  end
+  @target [17, 61]
+  @out_bins ["0", "1", "2"]
 
   def run(input \\ input()) do
-    ControlPanel.start_servers()
+    {rules, inventory} = parse(input)
 
-    input
-    |> parse()
-    |> ControlPanel.message_bots()
+    starting_bots =
+      inventory
+      |> Enum.filter(fn {_, chips} -> length(chips) == 2 end)
+      |> Enum.map(fn {id, _} -> id end)
 
-    {run_1(), run_2()}
+    {part1, outputs} = solve_simulation(inventory, rules, starting_bots)
+
+    part2 =
+      @out_bins
+      |> Enum.map(&outputs[&1])
+      |> Enum.product()
+
+    {String.to_integer(part1), part2}
+  end
+
+  defp solve_simulation(inventory, rules, ready, outputs \\ %{}, part1 \\ nil) do
+    case ready do
+      [] ->
+        {part1, outputs}
+
+      [bot_id | tail] ->
+        chips = Enum.sort(inventory[bot_id])
+        [low_v, high_v] = chips
+        {low_target, high_target} = rules[bot_id]
+
+        new_part1 = if chips == @target, do: bot_id, else: part1
+
+        {new_inv, new_out, low_ready} = give(low_v, low_target, inventory, outputs)
+        {new_inv, new_out, high_ready} = give(high_v, high_target, new_inv, new_out)
+        new_inv = Map.delete(new_inv, bot_id)
+
+        new_ready = tail ++ low_ready ++ high_ready
+
+        solve_simulation(new_inv, rules, new_ready, new_out, new_part1)
+    end
+  end
+
+  defp give(val, {type, id}, inventory, outputs) do
+    if type == "output" do
+      {inventory, Map.put(outputs, id, val), []}
+    else
+      existing = inventory[id] || []
+      new_chips = [val | existing]
+      new_inventory = Map.put(inventory, id, new_chips)
+      ready = if length(new_chips) == 2, do: [id], else: []
+      {new_inventory, outputs, ready}
+    end
   end
 
   def parse(data) do
-    lines = Transformers.lines(data)
-    configurations = lines |> Enum.map(&parse_configuration/1) |> Enum.reject(&is_nil/1)
-    assignments = lines |> Enum.map(&parse_assignment/1) |> Enum.reject(&is_nil/1)
+    data
+    |> Transformers.lines()
+    |> Enum.reduce({%{}, %{}}, fn line, {rules, inventory} ->
+      cond do
+        line =~ "gives low to" ->
+          [bot, low_type, low_id, high_type, high_id] =
+            Regex.run(
+              ~r/bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)/,
+              line,
+              capture: :all_but_first
+            )
 
-    configurations
-    |> Kernel.++(assignments)
-    |> tap(fn instructions ->
-      instructions
-      |> parse_names()
-      |> ControlPanel.create_bots()
-    end)
-  end
+          {Map.put(rules, bot, {{low_type, low_id}, {high_type, high_id}}), inventory}
 
-  @configuration ~r"bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)"
-  defp parse_configuration(line) do
-    @configuration
-    |> Regex.run(line, capture: :all_but_first)
-    |> then(fn
-      nil -> nil
-      [bot, lo_sink, lo, hi_sink, hi] -> {bot, %{low: {lo_sink, lo}, high: {hi_sink, hi}}}
-    end)
-  end
+        line =~ "value" ->
+          [val, bot] = Regex.run(~r/value (\d+) goes to bot (\d+)/, line, capture: :all_but_first)
 
-  @assignment ~r"value (\d+) goes to bot (\d+)"
-  defp parse_assignment(line) do
-    @assignment
-    |> Regex.run(line, capture: :all_but_first)
-    |> then(fn
-      nil -> nil
-      [val, bot] -> {bot, String.to_integer(val)}
-    end)
-  end
-
-  def parse_names(lines) do
-    Enum.flat_map(lines, fn
-      {id_1, %{high: {sink_2, id_2}, low: {sink_3, id_3}}} ->
-        [id_1, (sink_2 === "bot" && id_2) || nil, (sink_3 && id_3) || nil]
-
-      {id, _} ->
-        [id]
+          {rules,
+           Map.update(inventory, bot, [String.to_integer(val)], &[String.to_integer(val) | &1])}
+      end
     end)
   end
 end
