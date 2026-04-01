640bcfee42177f42439d4b63507ac4f427dbe4d1
Mafinar Khan <mafinar@gmail.com>
2026-04-01 00:09:54 -0400

    Remove DisjointSet impl

--- test/algorithms/disjoint_set_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AdventOfCode.Algorithms.DisjointSetTest do
-  use ExUnit.Case, async: true
-  @moduletag :algorithm_disjoint_set
-
-  alias AdventOfCode.Algorithms.DisjointSet
-
-  doctest AdventOfCode.Algorithms.DisjointSet
-end
