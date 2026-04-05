9818f5dbb7228d50f7c6c3a7029f28a81396091d
Mafinar Khan <mafinar@gmail.com>
2026-04-05 18:42:15 -0400

    Refactor 2023/17

--- lib/2023/day_17.ex
+++ lib/2023/day_17.ex
@@ -11,7 +11,7 @@ defmodule AdventOfCode.Y2023.Day17 do
   def input, do: InputReader.read_from_file(2023, 17)
 
   def run(input \\ input()) do
-    grid = parse_grid(input)
+    grid = parse(input)
     {max_x, max_y} = grid |> Map.keys() |> Enum.max()
 
     p1 = solve(grid, max_x, max_y, 0, 3)
@@ -21,12 +21,9 @@ defmodule AdventOfCode.Y2023.Day17 do
   end
 
   defp solve(grid, max_x, max_y, min_turn, max_straight) do
-    # State: {x, y, dir_atom, consecutive_count}
-    # dir_atom: :r, :l, :d, :u, :start
     Dijkstra.implicit_dijkstra_by(
       from: {0, 0, :start, 0},
       successors_with_cost: fn {x, y, dir, count} ->
-        # Possible directions to try
         dirs = if dir == :start, do: [:r, :d], else: next_dirs(dir, count, min_turn, max_straight)
 
         Enum.flat_map(dirs, fn d ->
@@ -34,7 +31,6 @@ defmodule AdventOfCode.Y2023.Day17 do
 
           case Map.fetch(grid, {nx, ny}) do
             {:ok, cost} ->
-              # New count is count + 1 if straight, else 1
               ncount = if d == dir, do: count + 1, else: 1
               [{{nx, ny, d, ncount}, cost}]
 
@@ -55,14 +51,7 @@ defmodule AdventOfCode.Y2023.Day17 do
   end
 
   defp next_dirs(dir, count, min_turn, max_straight) do
-    # Rules:
-    # 1. Can always go straight if count < max_straight
-    # 2. Can turn if count >= min_turn
-    # 3. NO REVERSE
-
-    # Straight?
     straight = if count < max_straight, do: [dir], else: []
-    # Turns?
     turns = if count >= min_turn, do: turns(dir), else: []
 
     straight ++ turns
@@ -78,19 +67,17 @@ defmodule AdventOfCode.Y2023.Day17 do
   defp move(x, y, :u), do: {x, y - 1}
   defp move(x, y, :d), do: {x, y + 1}
 
-  defp parse_grid(input) do
+  defp parse(input) do
     input
     |> Transformers.lines()
     |> Enum.with_index()
-    |> Enum.reduce(%{}, fn {line, y}, grid ->
+    |> List.foldl(%{}, fn {line, y}, grid ->
       line
       |> String.graphemes()
       |> Enum.with_index()
-      |> Enum.reduce(grid, fn {char, x}, acc ->
+      |> List.foldl(grid, fn {char, x}, acc ->
         Map.put(acc, {x, y}, String.to_integer(char))
       end)
     end)
   end
-
-  def parse(data \\ input()), do: data
 end
